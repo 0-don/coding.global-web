63ad5a410b566db54c72bc18eb62889580bce812
Don-Cryptus <don.cryptus@gmail.com>
2023-03-31 20:05:17 +0200

    test

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,5 @@ import solid from 'solid-start/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [solid({ ssr: true })],
-  optimizeDeps: {
-    disabled: true,
-  },
+  plugins: [solid({ ssr: false })],
 });
