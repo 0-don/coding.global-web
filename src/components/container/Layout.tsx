006d01daec5a32e7ad24fe35e5623dfe32cf960e
Don-Cryptus <don.cryptus@gmail.com>
2023-03-31 16:55:57 +0200

    test

--- src/components/container/Layout.tsx
+++ src/components/container/Layout.tsx
@@ -3,10 +3,10 @@ import { JSX } from 'solid-js';
 export const Layout = ({ children }: { children: JSX.Element }) => {
   return (
     <>
-      <img
+      {/* <img
         src='/images/banner.webp'
         class='img-fade relative h-[50vh] w-full object-cover object-top opacity-20 md:h-[100vh]'
-      />
+      /> */}
       <div class='absolute bottom-0 left-0 right-0 top-0'>{children}</div>
     </>
   );
