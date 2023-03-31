aab1a4e1b779e74ab1730001fed98d7a74913786
Don-Cryptus <don.cryptus@gmail.com>
2023-03-31 17:08:19 +0200

    test

--- src/components/container/Layout.tsx
+++ src/components/container/Layout.tsx
@@ -1,14 +1,22 @@
-import { JSX, children as useChildren } from 'solid-js';
+import { Component, JSX } from 'solid-js';
 
-export const Layout = ({ children }: { children: JSX.Element }) => {
-  const c = useChildren(() => children);
+interface LayoutProps {
+  children: JSX.Element;
+}
+
+export const Layout: Component<LayoutProps> = (props) => {
+  const children = Array.isArray(props.children)
+    ? props.children
+    : [props.children];
   return (
     <>
       <img
         src='/images/banner.webp'
         class='img-fade relative h-[50vh] w-full object-cover object-top opacity-20 md:h-[100vh]'
       />
-      <div class='absolute bottom-0 left-0 right-0 top-0'>{c()}</div>
+      <div class='absolute bottom-0 left-0 right-0 top-0'>
+        {children.map((child) => child)}
+      </div>
     </>
   );
 };
