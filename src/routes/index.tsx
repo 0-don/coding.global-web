d7fdced7f6bd4acebd41ccb6aab9f4792e7954d2
kryptn36 <51272147+kryptn36@users.noreply.github.com>
2023-04-01 19:31:22 +0200

    :art: Fix don's layout

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -13,9 +13,8 @@ export default function Home() {
       />
 
       <Layout>
-        <div class='flex w-full h-full flex-col items-center justify-center text-center lg:min-h-screen'>
-          <div class="mt-auto"></div>
-          <UnstyledLink href='https://discord.gg/coding'>
+        <div class='flex w-full h-full flex-col items-center text-center'>
+          <UnstyledLink className="mt-auto" href='https://discord.gg/coding'>
             <div class='flex h-28 w-28 justify-center items-center rounded-full bg-white pb-1 text-center text-6xl font-medium'>
               <span class='text-red-600'>{'<'}</span>
               <span class='text-black'>/</span>
