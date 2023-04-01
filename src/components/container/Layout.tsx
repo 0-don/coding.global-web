574bb496461f799449f51933424125ab913be7ed
Don-Cryptus <don.cryptus@gmail.com>
2023-04-01 18:50:37 +0200

    asd

--- src/components/container/Layout.tsx
+++ src/components/container/Layout.tsx
@@ -5,18 +5,13 @@ interface LayoutProps {
 }
 
 export const Layout: Component<LayoutProps> = (props) => {
-  const children = Array.isArray(props.children)
-    ? props.children
-    : [props.children];
   return (
     <>
       <img
         src='/images/banner.webp'
         class='img-fade relative h-[50vh] w-full object-cover object-top opacity-20 md:h-[100vh]'
       />
-      <div class='absolute bottom-0 left-0 right-0 top-0'>
-        {children.map((child) => child)}
-      </div>
+      <div class='absolute bottom-0 left-0 right-0 top-0'>{props.children}</div>
     </>
   );
 };
