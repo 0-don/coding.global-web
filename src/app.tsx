9ce38d74808179f8acbb708905048907a3c56415
don.cryptus <don.cryptus@gmail.com>
2023-12-27 20:23:29 +0100

    update meta

--- src/app.tsx
+++ src/app.tsx
@@ -2,7 +2,6 @@
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start";
 import { Suspense } from "solid-js";
-
 import "./app.css";
 
 export default function App() {
