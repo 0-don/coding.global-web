ba2981fa70b2ca308ab25c27f95741ad61ea4b1d
bit-barron <1bit.baron@gmail.com>
2024-03-17 01:55:15 +0100

    clean up & fixed team page

--- src/components/elements/Header.tsx
+++ src/components/elements/Header.tsx
@@ -1,6 +1,6 @@
-import { A } from '@solidjs/router';
-import { Component } from 'solid-js';
-
+import { A } from "@solidjs/router";
+import { Component } from "solid-js";
+import { Button } from "~/components/ui/button";
 
 interface HeaderProps {
   name: string;
@@ -8,14 +8,14 @@ interface HeaderProps {
 
 export const Header: Component<HeaderProps> = ({ name }) => {
   return (
-    <div class='flex items-center justify-between'>
-      <A href='/'>
-        <button class='rounded bg-blue-500 font-bold hover:bg-blue-700'>
-          Home
+    <div class="flex items-center justify-between mt-6">
+      <A href="/">
+        <button class="rounded bg-blue-500 font-bold hover:bg-blue-700">
+          <Button variant="outline">Home</Button>
         </button>
       </A>
 
-      <h1 class='font-secondary text-center text-white'>{name}</h1>
+      <h1 class="font-secondary text-center text-white">{name}</h1>
       <div></div>
     </div>
   );
