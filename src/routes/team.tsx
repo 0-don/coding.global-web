6a17d74a6909c601877d9b788676eb1060130a61
bit-barron <1bit.baron@gmail.com>
2024-03-17 02:23:00 +0100

    update

--- src/routes/team.tsx
+++ src/routes/team.tsx
@@ -1,10 +1,29 @@
+import { Staff } from "~/types";
 import { Layout } from "../components/container/Layout";
 import { Header } from "../components/elements/Header";
-import { MemberDisplay } from "../components/elements/MemberDisplay";
 import { TitleDescription } from "../components/seo/TitleDescription";
-import { teamMembers } from "../utils/teamMembers";
+import { createSignal } from "solid-js";
+import { onMount } from "solid-js";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
 
 export default function Team() {
+  const [members, setMembers] = createSignal<Staff[]>([]);
+
+  onMount(async () => {
+    const res = await fetch(
+      "https://bot.coding.global/api/693908458986143824/staff",
+    );
+    const data: Staff[] = await res.json();
+    console.log(data);
+    setMembers(data);
+  });
+
   return (
     <>
       <TitleDescription title="Team" description="Coding discord Team" />
@@ -14,8 +33,25 @@ export default function Team() {
           <Header name="Team" />
 
           <div class="mt-10 gap-10 md:grid md:grid-cols-3">
-            {teamMembers.map((m) => (
-              <MemberDisplay unresolvedMember={m} />
+            {members().map((m) => (
+              <div class="">
+                <Card class="">
+                  <CardHeader>
+                    <img
+                      src={m.avatarUrl}
+                      class="rounded-full"
+                      alt={m.username}
+                      height={100}
+                      width={100}
+                    />
+                    <CardDescription>{m.username}</CardDescription>
+                    <CardDescription>{m.staffRoles}</CardDescription>
+                  </CardHeader>
+                  <CardContent>
+                    <CardTitle>{m.guildName}</CardTitle>
+                  </CardContent>
+                </Card>
+              </div>
             ))}
           </div>
         </section>
