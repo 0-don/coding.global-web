c4e00b3af4fc56185bf64b28483554e59b9af25a
don.cryptus <don.cryptus@gmail.com>
2024-03-20 23:02:47 +0100

    easy

--- src/routes/api/db.ts
+++ src/routes/api/db.ts
@@ -1,19 +1,7 @@
-import { Type as t } from "@sinclair/typebox/type";
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
-import { parse } from "~/utils";
 import * as schema from "./schema";
-
-const serverEnvSchema = t.Object({
-  DATABASE_URL: t.String({
-    minLength: 1,
-    error: "DATABASE_URL server environment variable is not set!",
-  }),
-});
-
-const serverEnv = parse(serverEnvSchema, {
-  DATABASE_URL: import.meta.env.DATABASE_URL,
-});
+import { serverEnv } from "~/utils/env/server";
 
 export const db = drizzle(postgres(serverEnv.DATABASE_URL), {
   schema,
--- src/utils/env/server/index.ts
+++ src/utils/env/server/index.ts
@@ -1,12 +1,15 @@
-import { Type as t } from '@sinclair/typebox/type';
-import { parse } from '~/utils';
+import { Type as t } from "@sinclair/typebox/type";
+import "dotenv/config";
+import { parse } from "~/utils";
 
 const serverEnvSchema = t.Object({
   DATABASE_URL: t.String({
     minLength: 1,
-    error: 'DATABASE_URL server environment variable is not set!',
+    error: "DATABASE_URL server environment variable is not set!",
   }),
 });
+
+console.log(import.meta.env);
 
 export const serverEnv = parse(serverEnvSchema, {
   DATABASE_URL: process.env.DATABASE_URL,
