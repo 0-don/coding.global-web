864cf94c64a73ceb2b0582b744b29483f5be31e0
don.cryptus <don.cryptus@gmail.com>
2024-03-21 00:06:47 +0100

    easy

--- src/utils/db.migrations/0000_good_garia.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS "comment" (
-	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
-	"user" text NOT NULL,
-	"content" text NOT NULL,
-	"createdAt" timestamp DEFAULT now()
-);
