0b0775ebabe355758effb11763ef7796045d1a4d
don.cryptus <don.cryptus@gmail.com>
2024-03-20 23:59:28 +0100

    easy

--- src/routes/api/comment/index.ts
+++ src/routes/api/comment/index.ts
@@ -4,7 +4,7 @@ import { db } from "~/routes/api/db";
 import { comment, commentInsertSchema, commentSelectSchema } from "./schema";
 
 export const commentRoute = new Elysia({ prefix: "/comment" })
-  .get("", async () => ["await db.select().from(comment)"])
+  .get("", async () => await db.select().from(comment))
   .post("", async ({ body }) => await db.insert(comment).values(body), {
     body: commentInsertSchema,
   })
