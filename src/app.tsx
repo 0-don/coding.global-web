2dc80f25f133cee85127651d884ef242b9e7bf9f
don.cryptus <don.cryptus@gmail.com>
2024-03-20 20:58:21 +0100

    easy

--- src/app.tsx
+++ src/app.tsx
@@ -6,6 +6,11 @@ import {
 import { MetaProvider, Title } from "@solidjs/meta";
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
+import {
+  MutationCache,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/solid-query";
 import { ErrorBoundary, Suspense } from "solid-js";
 import { isServer } from "solid-js/web";
 import { getCookie } from "vinxi/server";
@@ -22,26 +27,41 @@ export default function App() {
   const storageManager = cookieStorageManagerSSR(
     isServer ? getServerCookies() : document.cookie,
   );
+  
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: false,
+        refetchOnWindowFocus: false,
+        placeholderData: (previousData: unknown) => previousData,
+      },
+    },
+    mutationCache: new MutationCache({
+      onSuccess: async () => void (await queryClient.invalidateQueries()),
+    }),
+  });
 
   return (
     <Router
       root={(props) => (
         <MetaProvider>
-          <Title>coding.global</Title>
-          <ColorModeScript storageType={storageManager.type} />
-          <ErrorBoundary
-            fallback={(err) => {
-              console.error(err);
-              return err;
-            }}
-          >
-            <ColorModeProvider storageManager={storageManager}>
-              <div class="absolute right-0 top-0 z-[9999]">
-                <ModeToggle />
-              </div>
-              <Suspense>{props.children}</Suspense>
-            </ColorModeProvider>
-          </ErrorBoundary>
+          <QueryClientProvider client={queryClient}>
+            <Title>coding.global</Title>
+            <ColorModeScript storageType={storageManager.type} />
+            <ErrorBoundary
+              fallback={(err) => {
+                console.error(err);
+                return err;
+              }}
+            >
+              <ColorModeProvider storageManager={storageManager}>
+                <div class="absolute right-0 top-0 z-[9999]">
+                  <ModeToggle />
+                </div>
+                <Suspense>{props.children}</Suspense>
+              </ColorModeProvider>
+            </ErrorBoundary>
+          </QueryClientProvider>
         </MetaProvider>
       )}
     >
