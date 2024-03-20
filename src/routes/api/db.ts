e6ec635ecb92dc83ad26c63133d73267f47dcf81
don.cryptus <don.cryptus@gmail.com>
2024-03-21 00:06:47 +0100

    easy

--- src/routes/api/db.ts
+++ src/routes/api/db.ts
@@ -3,8 +3,6 @@ import postgres from "postgres";
 import { serverEnv } from "~/utils/env/server";
 import * as schema from "./schema";
 
-console.log("serverEnv.DATABASE_URL", serverEnv.DATABASE_URL);
-
 export const db = drizzle(postgres(serverEnv.DATABASE_URL), {
   schema,
   logger: import.meta.env.DEV,
