91cef43234b5da1d8dbfc674162ca16106ef1d6a
don.cryptus <don.cryptus@gmail.com>
2024-03-21 00:02:18 +0100

    easy

--- src/routes/api/db.ts
+++ src/routes/api/db.ts
@@ -1,7 +1,9 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
-import * as schema from "./schema";
 import { serverEnv } from "~/utils/env/server";
+import * as schema from "./schema";
+
+console.log("serverEnv.DATABASE_URL", serverEnv.DATABASE_URL);
 
 export const db = drizzle(postgres(serverEnv.DATABASE_URL), {
   schema,
