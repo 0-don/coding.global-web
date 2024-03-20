2690696831cb58caccd7b568ad1b196a8d461746
don.cryptus <don.cryptus@gmail.com>
2024-03-20 21:26:59 +0100

    easy

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,6 +3,8 @@ import type { Config } from "drizzle-kit";
 if (!process.env.DATABASE_URL)
   throw new Error("DATABASE_URL environment variable is not set!");
 
+console.log("DATABASE_URL: ", process.env.DATABASE_URL);
+
 export default {
   driver: "pg",
   schema: "./src/routes/api/schema.ts",
