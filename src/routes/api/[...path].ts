18c8638074c754a67235fa79a980bfaa91f41531
don.cryptus <don.cryptus@gmail.com>
2024-03-20 23:02:47 +0100

    easy

---
+++ src/routes/api/[...path].ts
@@ -0,0 +1,10 @@
+import { APIEvent } from "@solidjs/start/server";
+import { app } from "~/routes/api";
+
+const handler = async (event: APIEvent) => await app.handle(event.request);
+
+export const GET = handler;
+export const POST = handler;
+export const PUT = handler;
+export const PATCH = handler;
+export const DELETE = handler;
