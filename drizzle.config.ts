846ba7bf264fd1816f6c604a925082b2574536d6
don.cryptus <don.cryptus@gmail.com>
2024-03-21 01:30:16 +0100

    asd

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,16 +1,7 @@
 import type { Config } from "drizzle-kit";
-import postgres from "postgres";
 
 if (!process.env.DATABASE_URL)
   throw new Error("DATABASE_URL environment variable is not set!");
-
-const pg = postgres(process.env.DATABASE_URL);
-
-const createDatabaseIfNotExists = async (dbName: string) =>
-  (await pg`SELECT 1 FROM pg_database WHERE datname = ${dbName}`).count === 0 &&
-  (await pg.unsafe(`CREATE DATABASE ${dbName}`));
-
-createDatabaseIfNotExists(process.env.POSTGRES_DB);
 
 export default {
   driver: "pg",
