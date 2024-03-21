88c26ad17578fd3ef052a84e34c98e6197eca565
don.cryptus <don.cryptus@gmail.com>
2024-03-21 01:30:16 +0100

    asd

--- src/routes/api/db.ts
+++ src/routes/api/db.ts
@@ -3,6 +3,14 @@ import postgres from "postgres";
 import { serverEnv } from "~/utils/env/server";
 import * as schema from "./schema";
 
+const pg = postgres(process.env.DATABASE_URL);
+
+const createDatabaseIfNotExists = async (dbName: string) =>
+  (await pg`SELECT 1 FROM pg_database WHERE datname = ${dbName}`).count === 0 &&
+  (await pg.unsafe(`CREATE DATABASE ${dbName}`));
+
+await createDatabaseIfNotExists(process.env.POSTGRES_DB);
+
 export const db = drizzle(postgres(serverEnv.DATABASE_URL), {
   schema,
   logger: import.meta.env.DEV,
