33c6a82e0ff4772c0453faf5007d3c1312c7355a
don.cryptus <don.cryptus@gmail.com>
2024-03-21 11:59:39 +0100

    test

--- src/routes/api/db.ts
+++ src/routes/api/db.ts
@@ -5,6 +5,8 @@ import postgres from "postgres";
 import { serverEnv } from "~/utils/env/server";
 import * as schema from "./schema";
 
+console.log("DATABASE_URL", serverEnv.DATABASE_URL);
+
 const connection = postgres(serverEnv.DATABASE_URL);
 
 export const db = drizzle(connection, { schema, logger: import.meta.env.DEV });
