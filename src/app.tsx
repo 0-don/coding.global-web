ded41dba365be8dd1bcb517ad85e3f8dd3e734cf
don.cryptus <don.cryptus@gmail.com>
2024-03-22 20:29:05 +0100

    easy

--- src/app.tsx
+++ src/app.tsx
@@ -1,64 +1,27 @@
 import { edenTreaty } from "@elysiajs/eden";
-import {
-  COLOR_MODE_STORAGE_KEY,
-  ColorModeProvider,
-  ColorModeScript,
-  cookieStorageManagerSSR,
-} from "@kobalte/core";
-import { SessionProvider } from "@solid-mediakit/auth/client";
 import { MetaProvider, Title } from "@solidjs/meta";
 import { Router } from "@solidjs/router";
 import { clientOnly } from "@solidjs/start";
 import { FileRoutes } from "@solidjs/start/router";
-import { QueryClient, QueryClientProvider } from "@tanstack/solid-query";
 import { SolidQueryDevtools } from "@tanstack/solid-query-devtools";
-import { Suspense } from "solid-js";
-import { isServer } from "solid-js/web";
-import { getCookie } from "vinxi/server";
 import "./app.css";
+import { Providers } from "./providers";
 import type { App } from "./routes/api";
 import { clientEnv } from "./utils/env/client";
 
-function getServerCookies() {
-  "use server";
-  const colorMode = getCookie(COLOR_MODE_STORAGE_KEY);
-  return colorMode ? `${COLOR_MODE_STORAGE_KEY}=${colorMode}` : "";
-}
-
 export const rpc = edenTreaty<App>(clientEnv.HOST_URL);
-
-const LanguageProvider = clientOnly(
-  () => import("./components/provider/language-provider"),
-);
 
 const Toaster = clientOnly(() => import("./components/ui/toast"));
 
 export default function App() {
-  const storageManager = cookieStorageManagerSSR(
-    isServer ? getServerCookies() : document.cookie,
-  );
-
-  const queryClient = new QueryClient({
-    defaultOptions: { queries: { refetchOnWindowFocus: false } },
-  });
-
   return (
     <Router
       root={(props) => (
         <MetaProvider>
-          <SessionProvider>
-            <QueryClientProvider client={queryClient}>
-              <SolidQueryDevtools initialIsOpen={false} />
-              <Title>coding.global</Title>
-              <Toaster />
-              <ColorModeScript storageType={storageManager.type} />
-              <ColorModeProvider storageManager={storageManager}>
-                <LanguageProvider>
-                  <Suspense>{props.children}</Suspense>
-                </LanguageProvider>
-              </ColorModeProvider>
-            </QueryClientProvider>
-          </SessionProvider>
+          <SolidQueryDevtools initialIsOpen={false} />
+          <Title>coding.global</Title>
+          <Toaster />
+          <Providers>{props.children}</Providers>
         </MetaProvider>
       )}
     >
---
+++ src/providers.tsx
@@ -0,0 +1,53 @@
+import {
+  COLOR_MODE_STORAGE_KEY,
+  ColorModeProvider,
+  ColorModeScript,
+  cookieStorageManagerSSR,
+} from "@kobalte/core";
+import { SessionProvider } from "@solid-mediakit/auth/client";
+import { clientOnly } from "@solidjs/start";
+import {
+  QueryClient,
+  QueryClientProvider,
+  isServer,
+} from "@tanstack/solid-query";
+import { Component, JSX, Suspense } from "solid-js";
+import { getCookie } from "vinxi/http";
+
+interface ProvidersProps {
+  children: JSX.Element;
+}
+
+function getServerCookies() {
+  "use server";
+  const colorMode = getCookie(COLOR_MODE_STORAGE_KEY);
+  return colorMode ? `${COLOR_MODE_STORAGE_KEY}=${colorMode}` : "";
+}
+
+const LanguageProvider = clientOnly(
+  () => import("./components/provider/language-provider"),
+);
+
+export const Providers: Component<ProvidersProps> = (props) => {
+  const storageManager = cookieStorageManagerSSR(
+    isServer ? getServerCookies() : document.cookie,
+  );
+
+  const queryClient = new QueryClient({
+    defaultOptions: { queries: { refetchOnWindowFocus: false } },
+  });
+  return (
+    <>
+      <ColorModeScript storageType={storageManager.type} />
+      <SessionProvider>
+        <QueryClientProvider client={queryClient}>
+          <ColorModeProvider storageManager={storageManager}>
+            <LanguageProvider>
+              <Suspense>{props.children}</Suspense>
+            </LanguageProvider>
+          </ColorModeProvider>
+        </QueryClientProvider>
+      </SessionProvider>
+    </>
+  );
+};
