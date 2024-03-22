35dd231c2be89e5f0e53c479805eadceb3cf68d3
don.cryptus <don.cryptus@gmail.com>
2024-03-22 21:16:43 +0100

    easy

--- src/utils/env/server/index.ts
+++ src/utils/env/server/index.ts
@@ -1,4 +1,6 @@
+import Discord from "@auth/core/providers/discord";
 import { Type as t } from "@sinclair/typebox/type";
+import { SolidAuthConfig } from "@solid-mediakit/auth";
 import { parse } from "~/utils";
 
 const serverEnvSchema = t.Object({
@@ -6,8 +8,28 @@ const serverEnvSchema = t.Object({
     minLength: 1,
     error: "DATABASE_URL server environment variable is not set!",
   }),
+  DISCORD_CLIENT_ID: t.String({
+    minLength: 1,
+    error: "DISCORD_CLIENT_ID server environment variable is not set!",
+  }),
+  DISCORD_CLIENT_SECRET: t.String({
+    minLength: 1,
+    error: "DISCORD_CLIENT_SECRET server environment variable is not set!",
+  }),
 });
 
 export const serverEnv = parse(serverEnvSchema, {
   DATABASE_URL: process.env.DATABASE_URL,
+  DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
+  DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
 });
+
+export const authOpts: SolidAuthConfig = {
+  providers: [
+    Discord({
+      clientId: serverEnv.DISCORD_CLIENT_ID,
+      clientSecret: serverEnv.DISCORD_CLIENT_SECRET,
+    }),
+  ],
+  debug: false,
+};
--- src/utils/types/environment.d.ts
+++ src/utils/types/environment.d.ts
@@ -4,7 +4,14 @@ declare namespace NodeJS {
     POSTGRES_PASSWORD: string;
     POSTGRES_HOST: string;
     POSTGRES_DB: string;
+    
     DATABASE_URL: string;
+
     VITE_HOST_URL: string;
+
+    DISCORD_CLIENT_ID: string;
+    DISCORD_CLIENT_SECRET: string;
+
+    BUN?: string;
   }
 }
