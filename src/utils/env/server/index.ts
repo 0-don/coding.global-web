f2703fe62a9c6df17a274dbf458d9ede0376bbbf
don.cryptus <don.cryptus@gmail.com>
2024-03-23 00:16:58 +0100

    easy

--- src/utils/env/server/index.ts
+++ src/utils/env/server/index.ts
@@ -30,13 +30,13 @@ export const authOpts: SolidAuthConfig = {
       clientId: serverEnv.DISCORD_CLIENT_ID,
       clientSecret: serverEnv.DISCORD_CLIENT_SECRET,
       authorization: {
-        params: { scope: ["identify",  "guilds", "email",].join(" ") },
+        params: { scope: ["identify"].join(" ") },
       },
     }),
   ],
+
   debug: false,
 };
 // https://discord.com/oauth2/authorize?scope=identify+email&response_type=code&client_id=1220819574661054665&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fapi%2Fauth%2Fcallback%2Fdiscord&code_challenge=t_0slsW20GV6Oe-El_GK_siAcEirbCcG0gDFOa3ltEc&code_challenge_method=S256
-
 
 // https://discord.com/oauth2/authorize?client_id=1220819574661054665&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth%2Fredirect&scope=identify+guilds+email
