ca8a18fb16d1db0a08d2b9082acfd5a4010e2f75
don.cryptus <don.cryptus@gmail.com>
2024-03-22 21:42:51 +0100

    easy

--- src/providers.tsx
+++ src/providers.tsx
@@ -1,7 +1,7 @@
 import { SessionProvider } from "@solid-mediakit/auth/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/solid-query";
 import { SolidQueryDevtools } from "@tanstack/solid-query-devtools";
-import { Component, JSX, Suspense } from "solid-js";
+import { Component, JSX } from "solid-js";
 import LanguageProvider from "./components/provider/language-provider";
 import { ThemeProvider } from "./components/provider/theme-provider";
 
@@ -9,25 +9,19 @@ interface ProvidersProps {
   children: JSX.Element;
 }
 
-// const LanguageProvider = clientOnly(
-//   () => import("./components/provider/language-provider"),
-// );
-
 export const Providers: Component<ProvidersProps> = (props) => {
   const queryClient = new QueryClient({
     defaultOptions: { queries: { refetchOnWindowFocus: false } },
   });
 
   return (
-    <Suspense>
-      <QueryClientProvider client={queryClient}>
-        <SolidQueryDevtools initialIsOpen={false} />
-        <ThemeProvider>
-          <LanguageProvider>
-            <SessionProvider>{props.children}</SessionProvider>
-          </LanguageProvider>
-        </ThemeProvider>
-      </QueryClientProvider>
-    </Suspense>
+    <QueryClientProvider client={queryClient}>
+      <SolidQueryDevtools initialIsOpen={false} />
+      <ThemeProvider>
+        <LanguageProvider>
+          <SessionProvider>{props.children}</SessionProvider>
+        </LanguageProvider>
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 };
