3c221e9145ae2eb0a42cd48d6127511361e39f1e
don.cryptus <don.cryptus@gmail.com>
2024-03-22 20:53:07 +0100

    easy

--- src/routes/api/auth/[...solidauth].ts
+++ src/routes/api/auth/[...solidauth].ts
@@ -1,6 +1,12 @@
 import Discord from "@auth/core/providers/discord";
 import { SolidAuth, type SolidAuthConfig } from "@solid-mediakit/auth";
 
+// console.log("process.env.DISCORD_CLIENT_ID", process.env.DISCORD_CLIENT_ID);
+// console.log(
+//   "process.env.DISCORD_CLIENT_SECRET",
+//   process.env.DISCORD_CLIENT_SECRET,
+// );
+
 export const authOpts: SolidAuthConfig = {
   providers: [
     Discord({
@@ -10,5 +16,7 @@ export const authOpts: SolidAuthConfig = {
   ],
   debug: false,
 };
+
+console.log("authOpts", authOpts);
 
 export const { GET, POST } = SolidAuth(authOpts);
