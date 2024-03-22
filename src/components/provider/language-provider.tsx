1eb7b7dfb2eb9eb5570577813fd0a35873b92dc7
don.cryptus <don.cryptus@gmail.com>
2024-03-22 22:36:59 +0100

    easy

--- src/components/provider/language-provider.tsx
+++ src/components/provider/language-provider.tsx
@@ -1,10 +1,10 @@
 import { JSX } from "solid-js";
 import { isServer } from "solid-js/web";
-import { getCookie, setCookie } from "vinxi/http";
+import { getCookie } from "vinxi/http";
 import TypesafeI18n from "~/i18n/i18n-solid";
 import { Locales } from "~/i18n/i18n-types";
 import { baseLocale } from "~/i18n/i18n-util";
-import { loadLocaleAsync } from "~/i18n/i18n-util.async";
+import { loadLocale } from "~/i18n/i18n-util.sync";
 import { parseCookie } from "~/utils";
 import { clientEnv } from "~/utils/env/client";
 
@@ -12,12 +12,12 @@ function getServerLanguageCookie() {
   "use server";
   const cookieValue = getCookie(clientEnv.LANGUAGE_KEY);
 
-  if (!cookieValue) {
-    setCookie(clientEnv.LANGUAGE_KEY, baseLocale, {
-      maxAge: 60 * 60 * 24 * 365,
-      path: "/",
-    });
-  }
+  // if (!cookieValue && isServer) {
+  //   setCookie(clientEnv.LANGUAGE_KEY, baseLocale, {
+  //     maxAge: 60 * 60 * 24 * 365,
+  //     path: "/",
+  //   });
+  // }
 
   return cookieValue
     ? `${clientEnv.LANGUAGE_KEY}=${cookieValue}`
@@ -29,7 +29,7 @@ export default function LanguageProvider(props: { children: JSX.Element }) {
   const detectedLocale =
     (parseCookie(cookie, clientEnv.LANGUAGE_KEY) as Locales) || baseLocale;
 
-  loadLocaleAsync(detectedLocale);
+  loadLocale(detectedLocale);
 
   return <TypesafeI18n locale={detectedLocale}>{props.children}</TypesafeI18n>;
 }
