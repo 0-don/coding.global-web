0a65dbeada43c1f27cb3f5b1002ba59885285db2
don.cryptus <don.cryptus@gmail.com>
2024-03-22 01:20:27 +0100

    ok

--- src/i18n/formatters.ts
+++ src/i18n/formatters.ts
@@ -1,12 +1,13 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import type { FormattersInitializer } from 'typesafe-i18n'
-import type { Locales, Formatters } from './i18n-types.js'
+import type { FormattersInitializer } from "typesafe-i18n";
+import type { Locales, Formatters } from "./i18n-types.js";
 
-export const initFormatters: FormattersInitializer<Locales, Formatters> = (_locale: Locales) => {
+export const initFormatters: FormattersInitializer<Locales, Formatters> = (
+  _locale: Locales,
+) => {
+  const formatters: Formatters = {
+    // add your formatter functions here
+  };
 
-	const formatters: Formatters = {
-		// add your formatter functions here
-	}
-
-	return formatters
-}
+  return formatters;
+};
