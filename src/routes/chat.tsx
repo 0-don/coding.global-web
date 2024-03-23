69624a57f2d8b9e874b076949a959cce4fa362e3
don.cryptus <don.cryptus@gmail.com>
2024-03-23 16:44:45 +0100

    test

--- src/routes/chat.tsx
+++ src/routes/chat.tsx
@@ -1,8 +1,7 @@
-import { SubmitHandler, createForm } from "@modular-forms/solid";
-import { Create } from "@sinclair/typebox/value";
 import { signIn } from "@solid-mediakit/auth/client";
 import { TbLoader, TbTrashXFilled } from "solid-icons/tb";
-import { For } from "solid-js";
+import { For, createSignal } from "solid-js";
+import { JSX } from "solid-js/web/types/jsx";
 import { Button } from "~/components/ui/button";
 import { Grid } from "~/components/ui/grid";
 import { Input } from "~/components/ui/input";
@@ -10,20 +9,15 @@ import { Label } from "~/components/ui/label";
 import { useI18nContext } from "~/i18n/i18n-solid";
 import { CommentHook } from "~/lib/hook/comment-hook";
 import { Layout } from "../components/container/layout";
-import {
-  CommentInsertSimple,
-  commentInsertSimpleSchema,
-} from "./api/comment/schema";
 
 export default function Chat() {
   const { LL } = useI18nContext();
   const { commentAdd, commentsQuery, commentDelete } = CommentHook();
-  const [authForm, { Form, Field }] = createForm({
-    initialValues: Create(commentInsertSimpleSchema),
-  });
+  const [content, setContent] = createSignal("");
 
-  const handleSubmit: SubmitHandler<CommentInsertSimple> = async (values) => {
-    await commentAdd.mutateAsync(values);
+  const handleSubmit: JSX.IntrinsicElements["form"]["onsubmit"] = async (e) => {
+    e.preventDefault();
+    await commentAdd.mutateAsync({ content: content() });
   };
 
   return (
@@ -40,31 +34,29 @@ export default function Chat() {
             </div>
           )}
         </For>
-        <Form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit}>
           <Grid class="gap-4">
-            <Field name="content">
-              {(_, props) => (
-                <Grid class="gap-1">
-                  <Label class="sr-only" for="content">
-                    Comment
-                  </Label>
-                  <Input
-                    {...props}
-                    id="content"
-                    type="text"
-                    placeholder="write your comment"
-                  />
-                </Grid>
-              )}
-            </Field>
-            <Button type="submit" disabled={authForm.submitting}>
-              {authForm.submitting && (
+            <Grid class="gap-1">
+              <Label class="sr-only" for="content">
+                Comment
+              </Label>
+              <Input
+                value={content()}
+                onInput={(e) => setContent(e.currentTarget.value)}
+                id="content"
+                type="text"
+                placeholder="write your comment"
+              />
+            </Grid>
+
+            <Button type="submit" disabled={commentAdd.isPending}>
+              {commentAdd.isPending && (
                 <TbLoader class="mr-2 size-4 animate-spin" />
               )}
               {LL().HI()}
             </Button>
           </Grid>
-        </Form>
+        </form>
         <Button onClick={() => signIn("discord")}>Login</Button>
       </section>
     </Layout>
