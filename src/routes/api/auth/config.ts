7714e2578781efe9810c58a2725c36c0a65c3ab4
don.cryptus <don.cryptus@gmail.com>
2024-03-23 13:46:54 +0100

    easy

--- src/routes/api/auth/config.ts
+++ src/routes/api/auth/config.ts
@@ -12,6 +12,16 @@ export const authOpts: SolidAuthConfig = {
       clientSecret: serverEnv.DISCORD_CLIENT_SECRET,
       authorization: "https://discord.com/api/oauth2/authorize?scope=identify",
       profile(profile) {
+        if (profile.avatar === null) {
+          const defaultAvatarNumber =
+            profile.discriminator === "0"
+              ? Number(BigInt(profile.id) >> BigInt(22)) % 6
+              : parseInt(profile.discriminator) % 5;
+          profile.image_url = `https://cdn.discordapp.com/embed/avatars/${defaultAvatarNumber}.png`;
+        } else {
+          const format = profile.avatar.startsWith("a_") ? "gif" : "png";
+          profile.image_url = `https://cdn.discordapp.com/avatars/${profile.id}/${profile.avatar}.${format}`;
+        }
         return {
           ...profile,
           id: profile.id,
