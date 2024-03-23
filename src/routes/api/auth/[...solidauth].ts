8fc8555adb98c7241deb693b0331cde27bde7cee
don.cryptus <don.cryptus@gmail.com>
2024-03-23 13:34:14 +0100

    easy

--- src/routes/api/auth/[...solidauth].ts
+++ src/routes/api/auth/[...solidauth].ts
@@ -3,7 +3,7 @@ import { SolidAuth, SolidAuthConfig } from "@solid-mediakit/auth";
 import { dbAdapter } from "~/routes/api/db";
 import { serverEnv } from "~/utils/env/server";
 
-export const authOpts: SolidAuthConfig = {
+const authOpts: SolidAuthConfig = {
   adapter: dbAdapter,
   providers: [
     Discord({
@@ -11,16 +11,6 @@ export const authOpts: SolidAuthConfig = {
       clientSecret: serverEnv.DISCORD_CLIENT_SECRET,
       authorization: "https://discord.com/api/oauth2/authorize?scope=identify",
       profile(profile) {
-        if (profile.avatar === null) {
-          const defaultAvatarNumber =
-            profile.discriminator === "0"
-              ? Number(BigInt(profile.id) >> BigInt(22)) % 6
-              : parseInt(profile.discriminator) % 5;
-          profile.image_url = `https://cdn.discordapp.com/embed/avatars/${defaultAvatarNumber}.png`;
-        } else {
-          const format = profile.avatar.startsWith("a_") ? "gif" : "png";
-          profile.image_url = `https://cdn.discordapp.com/avatars/${profile.id}/${profile.avatar}.${format}`;
-        }
         return {
           ...profile,
           id: profile.id,
@@ -31,25 +21,9 @@ export const authOpts: SolidAuthConfig = {
       },
     }),
   ],
-  // session: {
-  //   strategy: "database",
-  // },
-  callbacks: {
-    // jwt: async ({ account, token, user, profile, session }) => {
-    //   if (account) {
-    //     token.id = account.id;
-    //   }
-    //   return token;
-    // },
-    session: async ({ session }) => {
-      // console.log(token, user);
-
-      // console.log("session", session);
-      // session.user.id = user.id;
-      return session;
-    },
-  },
   debug: false,
 };
+
+export { authOpts };
 
 export const { GET, POST } = SolidAuth(authOpts);
