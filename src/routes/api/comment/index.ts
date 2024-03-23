8764f779b7dfc213d3a2d13e000ecc1c5309f5bb
don.cryptus <don.cryptus@gmail.com>
2024-03-23 17:27:10 +0100

    easy

--- src/routes/api/comment/index.ts
+++ src/routes/api/comment/index.ts
@@ -16,6 +16,7 @@ export const commentRoute = new Elysia({ prefix: "/comment" })
     async ({ body, request }) => {
       const session = await getSession(request, authOpts);
 
+      console.log(session);
       if (!session?.user?.name)
         throw new Error("You must be logged in to comment.");
 
