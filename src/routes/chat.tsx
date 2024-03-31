cc8ca6bfddf6698f39d943b4649b2ab029d1ed07
don.cryptus <don.cryptus@gmail.com>
2024-03-31 23:05:14 +0200

    test

--- src/routes/chat.tsx
+++ src/routes/chat.tsx
@@ -1,4 +1,5 @@
 import { clientOnly } from "@solidjs/start";
+import { Show, createSignal, onMount } from "solid-js";
 import { Header } from "~/components/container/header";
 import { ChatMessages } from "~/components/pages/chat/chat-messages";
 import { Card } from "~/components/ui/card";
@@ -9,12 +10,20 @@ const CreateMessage = clientOnly(
 );
 
 export default function Chat() {
+  const [init, setInit] = createSignal(false);
+
+  onMount(() => {
+    setInit(true);
+  });
+
   return (
     <Layout container class="mt-10 h-[calc(100vh-5rem)]">
       <Card class="flex h-full flex-col bg-secondary/85 p-10">
         <Header name="Chat" />
         <ChatMessages class="my-5 flex-1 overflow-y-auto" />
-        <CreateMessage />
+        <Show when={init()}>
+          <CreateMessage />
+        </Show>
       </Card>
     </Layout>
   );
