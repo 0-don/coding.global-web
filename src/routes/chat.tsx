25f1759e422e14558d2efa08c329ec88ab63e45f
don.cryptus <don.cryptus@gmail.com>
2024-03-31 21:10:01 +0200

    easy

--- src/routes/chat.tsx
+++ src/routes/chat.tsx
@@ -6,10 +6,12 @@ import { Layout } from "../components/container/layout";
 
 export default function Chat() {
   return (
-    <Layout container class="mt-10">
-      <Card class="bg-secondary p-10 ">
+    <Layout container class="mt-10 h-[calc(100vh-5rem)]">
+      <Card class="flex h-full flex-col justify-between bg-secondary/85 p-10">
         <Header name="Chat" />
-        <ChatMessages />
+        <div class="flex-1 overflow-y-auto">
+          <ChatMessages />
+        </div>
         <CreateMessage />
       </Card>
     </Layout>
