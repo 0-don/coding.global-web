a7292f99e9d94344aee91f8207743a17d85f18b1
don.cryptus <don.cryptus@gmail.com>
2024-03-31 22:33:19 +0200

    test

--- app.config.ts
+++ app.config.ts
@@ -2,25 +2,4 @@ import { defineConfig } from "@solidjs/start/config";
 
 export default defineConfig({
   server: process.env.BUN ? { preset: "bun" } : undefined,
-  // vite({ router }) {
-  //   if (router === "client") {
-  //     return {
-  //       server: {
-  //         hmr: {
-  //           port: 4000,
-  //         },
-  //         watch: {
-  //           usePolling: true,
-  //         },
-  //       },
-  //     };
-  //   }
-  //   return {
-  //     server: {
-  //       watch: {
-  //         usePolling: true,
-  //       },
-  //     },
-  //   };
-  // },
 });
