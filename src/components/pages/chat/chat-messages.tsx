738a7907a0dbd17e32eb7212184902886fc76a53
don.cryptus <don.cryptus@gmail.com>
2024-04-01 02:31:06 +0200

    easy

--- src/components/pages/chat/chat-messages.tsx
+++ src/components/pages/chat/chat-messages.tsx
@@ -1,57 +1,48 @@
-import { createSession, signIn } from "@solid-mediakit/auth/client";
-import { FaBrandsDiscord } from "solid-icons/fa";
-import { JSX, Show, createSignal } from "solid-js";
-import { Button } from "~/components/ui/button";
-import { Grid } from "~/components/ui/grid";
-import { Input } from "~/components/ui/input";
-import { Label } from "~/components/ui/label";
+import { createSession } from "@solid-mediakit/auth/client";
+import dayjs from "dayjs";
+import localizedFormat from "dayjs/plugin/localizedFormat";
+import { TbTrashXFilled } from "solid-icons/tb";
+import { Component, For, Show } from "solid-js";
 import { CommentHook } from "~/lib/hook/comment-hook";
 
-interface CreateMessageProps {
+dayjs.extend(localizedFormat);
+
+interface ChatMessagesProps {
   class?: string;
 }
 
-export default function CreateMessage(props: CreateMessageProps) {
-  const { commentAdd } = CommentHook();
-
+export const ChatMessages: Component<ChatMessagesProps> = (props) => {
+  const { commentsQuery, commentDelete } = CommentHook();
   const session = createSession();
-  const [content, setContent] = createSignal("");
-
-  const handleSubmit: JSX.IntrinsicElements["form"]["onsubmit"] = async (e) => {
-    e.preventDefault();
-    await commentAdd.mutateAsync({ content: content() });
-    setContent("");
-  };
 
   return (
-    <form onSubmit={handleSubmit} class={props.class}>
-      <Show when={session()}>
-        <Grid class="gap-1">
-          <Label class="sr-only" for="content">
-            Comment
-          </Label>
-          <Input
-            class="bg-gray-200 focus-visible:ring-0 focus-visible:ring-offset-0 dark:bg-zinc-700"
-            value={content()}
-            onInput={(e) => setContent(e.currentTarget.value)}
-            id="content"
-            type="text"
-            placeholder="write your comment"
-          />
-        </Grid>
-      </Show>
-      <Show when={!session()}>
-        <Button
-          type="button"
-          onClick={() => signIn("discord")}
-          class="w-full rounded-md bg-discord px-2 py-1 text-2xl font-black hover:bg-discord hover:opacity-90"
-        >
-          <div class="flex items-center text-white">
-            <FaBrandsDiscord />
-            <span>Discord Login</span>
+    <div class={props.class}>
+      <For each={commentsQuery.data}>
+        {(comment) => (
+          <div class="group my-2 flex w-full items-center space-x-2 truncate rounded-md p-1 hover:bg-zinc-200 hover:dark:bg-stone-900/20">
+            <Show when={comment?.user?.image}>
+              {(img) => <img class="h-14 rounded-full" src={img()} />}
+            </Show>
+            <div class="relative w-full">
+              <div class="flex cursor-default items-center space-x-2">
+                <p class="font-bold">{comment.user?.name}</p>
+                <p class="text-sm text-muted-foreground">
+                  {dayjs(comment.createdAt).format("L LT")}
+                </p>
+              </div>
+              <div>
+                <p>{comment.content}</p>
+                <Show when={comment?.user?.id === session()?.user?.me.id}>
+                  <TbTrashXFilled
+                    class="absolute right-0 top-0 hidden cursor-pointer hover:text-red-500 group-hover:block"
+                    onClick={() => commentDelete.mutateAsync(comment.id)}
+                  />
+                </Show>
+              </div>
+            </div>
           </div>
-        </Button>
-      </Show>
-    </form>
+        )}
+      </For>
+    </div>
   );
-}
+};
