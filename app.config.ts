ae3719e73cf20ac264a5facf4b890238f56ea43f
don.cryptus <don.cryptus@gmail.com>
2024-04-01 20:44:58 +0200

    test

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "@solidjs/start/config";
 
+console.log("process.env.BUN", process.env.BUN);
+
 export default defineConfig({
   ssr: false,
-  // server: process.env.BUN ? { preset: "bun" } : undefined,
+  server: process.env.BUN ? { preset: "bun" } : undefined,
 });
