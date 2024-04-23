12ab75e0ce91359c55d7f2878d38adf71b5aefac
don.cryptus <don.cryptus@gmail.com>
2024-04-23 22:27:44 +0200

    a

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "@solidjs/start/config";
 
 export default defineConfig({
-  ssr: false,
+  ssr: process.env.BUN ? true : false,
   server: process.env.BUN ? { preset: "bun" } : undefined,
+  vite: {
+    ssr: { external: ["@tanstack/solid-query"] },
+  },
 });
