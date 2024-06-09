fd58afa9b3c070eb94a09b1b1e0b7513df889f10
0-don <don.cryptus@gmail.com>
2024-06-10 01:13:45 +0200

    easy

--- app.config.ts
+++ app.config.ts
@@ -2,21 +2,14 @@ import { defineConfig } from "@solidjs/start/config";
 
 export default defineConfig({
   ssr: process.env.BUN ? true : false,
-  solid: {
-    babel: {},
-  },
   server: {
     preset: process.env.BUN ? "bun" : undefined,
-
-    esbuild: {
-      options: {},
-    },
     externals: {
       external: ["@auth/core", "@panva/hkdf"],
     },
   },
   vite: {
-    ssr: { external: ["@tanstack/solid-query"] },
+    ssr: { external: ["@tanstack/solid-query", "@auth/core", "@panva/hkdf"] },
     optimizeDeps: {
       include: ["@panva/hkdf"],
     },
