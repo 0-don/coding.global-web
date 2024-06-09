d2a19e59d2fe1b1b61d10321c7d991b1459145f8
0-don <don.cryptus@gmail.com>
2024-06-10 01:22:39 +0200

    easy

--- app.config.ts
+++ app.config.ts
@@ -4,12 +4,16 @@ export default defineConfig({
   ssr: process.env.BUN ? true : false,
   server: {
     preset: process.env.BUN ? "bun" : undefined,
+    rollupConfig: {
+      external: ["@auth/core", "@panva/hkdf"],
+    },
     externals: {
       external: ["@auth/core", "@panva/hkdf"],
     },
   },
   vite: {
     ssr: { external: ["@tanstack/solid-query", "@auth/core", "@panva/hkdf"] },
+
     optimizeDeps: {
       include: ["@panva/hkdf"],
     },
