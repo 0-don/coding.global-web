eecb97608d492d4fbd9a4778e61f1783d93078ca
0-don <don.cryptus@gmail.com>
2024-06-10 01:22:46 +0200

    easy

--- app.config.ts
+++ app.config.ts
@@ -13,7 +13,6 @@ export default defineConfig({
   },
   vite: {
     ssr: { external: ["@tanstack/solid-query", "@auth/core", "@panva/hkdf"] },
-
     optimizeDeps: {
       include: ["@panva/hkdf"],
     },
