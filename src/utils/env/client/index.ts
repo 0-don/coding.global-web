d6ce1aad4850ec9f591b71ee0cbdba6422dda4e1
0-don <don.cryptus@gmail.com>
2024-10-28 20:35:51 +0100

    easy

--- src/utils/env/client/index.ts
+++ src/utils/env/client/index.ts
@@ -20,9 +20,4 @@ const clientEnvSchema = t.Object({
   }),
 });
 
-export const clientEnv = parse(clientEnvSchema, {
-  HOST_URL: import.meta.env.VITE_HOST_URL,
-  STAFF_MEMERS_URL: "https://bot.coding.global/api/693908458986143824/staff",
-  LANGUAGE_KEY: "lang",
-  LANGUAGES: ["de", "en"],
-});
+export const clientEnv = parse(clientEnvSchema, import.meta.env);
