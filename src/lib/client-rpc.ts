82542dc8b2547ace5285f741ed2fb4c2762e9ca8
0-don <don.cryptus@gmail.com>
2024-10-28 23:41:54 +0100

    easy

---
+++ src/lib/client-rpc.ts
@@ -0,0 +1,8 @@
+import { edenTreaty } from "@elysiajs/eden";
+import type { App } from "~/routes/api";
+
+export const rpc = edenTreaty<App>(
+  typeof window === "undefined"
+    ? `http://localhost:${import.meta.env.PORT ?? 3000}`
+    : window.location.origin,
+);
