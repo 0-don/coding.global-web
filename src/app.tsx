749b4029755f69d9f1f69737782e5ac3bba8cf42
0-don <don.cryptus@gmail.com>
2024-10-28 23:43:56 +0100

    easy

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,10 @@
 // @refresh reload
-import { edenTreaty } from "@elysiajs/eden";
 import { MetaProvider, Title } from "@solidjs/meta";
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
 import "./app.css";
-import { Toaster } from "./components/ui/toast";
 import { Providers } from "./providers";
-import type { App } from "./routes/api";
-import { clientEnv } from "./utils/env/client";
-
 
 export default function App() {
   return (
@@ -17,7 +12,6 @@ export default function App() {
       root={(props) => (
         <MetaProvider>
           <Title>coding.global</Title>
-          <Toaster />
           <Suspense>
             <Providers>{props.children}</Providers>
           </Suspense>
