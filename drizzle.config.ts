d96880f12d8005961604e01cc1c07d265372e58f
0-don <don.cryptus@gmail.com>
2024-10-28 23:17:23 +0100

    easy

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,13 @@
+import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
+
+const DATABASE_URL = `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}/${process.env.POSTGRES_DB}`;
 
 export default defineConfig({
   dialect: "postgresql",
   schema: "./src/routes/api/schema.ts",
   out: "./db.migrations",
-  dbCredentials: { url: process.env.DATABASE_URL },
+  dbCredentials: { url: DATABASE_URL },
   // Print all statements
   verbose: false,
   // Always ask for confirmation
