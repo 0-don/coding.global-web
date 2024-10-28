0a0aaf5be2e85d60ef8fe260e6244317ff5cf081
0-don <don.cryptus@gmail.com>
2024-10-29 00:33:04 +0100

    easy

--- src/components/pages/chat/create-message.tsx
+++ src/components/pages/chat/create-message.tsx
@@ -1,3 +1,4 @@
+import { useAuth } from "@solid-mediakit/auth/client";
 import { FaBrandsDiscord } from "solid-icons/fa";
 import { JSX, Show, createSignal } from "solid-js";
 import { CommentHook } from "~/components/hook/comment-hook";
@@ -13,7 +14,7 @@ interface CreateMessageProps {
 export default function CreateMessage(props: CreateMessageProps) {
   const { commentAdd } = CommentHook();
   const [content, setContent] = createSignal("");
-  // const auth = useAuth();
+  const auth = useAuth();
 
   const handleSubmit: JSX.IntrinsicElements["form"]["onsubmit"] = async (e) => {
     e.preventDefault();
@@ -23,7 +24,7 @@ export default function CreateMessage(props: CreateMessageProps) {
 
   return (
     <form onSubmit={handleSubmit} class={props.class}>
-      <Show when={1}>
+      <Show when={auth.session()}>
         <Grid class="gap-1">
           <Label class="sr-only" for="content">
             Comment
@@ -38,10 +39,10 @@ export default function CreateMessage(props: CreateMessageProps) {
           />
         </Grid>
       </Show>
-      <Show when={1}>
+      <Show when={!auth.session()}>
         <Button
           type="button"
-          onClick={() => 1}
+          onClick={() => auth.signIn("discord")}
           class="w-full rounded-md bg-discord px-2 py-1 text-2xl font-black hover:bg-discord hover:opacity-90"
         >
           <div class="flex items-center text-white">
--- src/server/comment/route.ts
+++ src/server/comment/route.ts
@@ -1,15 +1,69 @@
-import { Elysia } from "elysia";
+import { getSession } from "@solid-mediakit/auth";
+import { and, eq, getTableColumns } from "drizzle-orm";
+import Elysia, { t } from "elysia";
+import { authOpts } from "~/routes/api/auth/auth-config";
+import { users } from "~/routes/api/auth/schema";
+import { db } from "~/routes/api/db";
+import {
+  comment,
+  commentInsertSimpleSchema,
+  commentSelectSchema,
+} from "./schema";
 
-export const commentRoute = new Elysia({ prefix: "/comment" }).get(
-  "",
-  async () => {
-    // const { email, ...user } = getTableColumns(users);
+export const commentRoute = new Elysia({ prefix: "/comment" })
+  .get("", async () => {
+    const { email, ...user } = getTableColumns(users);
+    return await db
+      .select({ ...getTableColumns(comment), user })
+      .from(comment)
+      .leftJoin(users, eq(comment.userId, users.id));
+  })
+  .post(
+    "",
+    async ({ body, request }) => {
+      const session = await getSession(request, authOpts);
 
-    // const comments = await db
-    //   .select({ ...getTableColumns(comment), user })
-    //   .from(comment)
-    //   .leftJoin(users, eq(comment.userId, users.id));
+      if (!session?.user?.name)
+        throw new Error("You must be logged in to comment.");
 
-    return "comments";
-  },
-);
+      const newComment = (
+        await db
+          .insert(comment)
+          .values({ ...body, userId: session.user.me.id! })
+          .returning()
+      ).at(0)!;
+
+      const { email, ...user } = getTableColumns(users);
+
+      return (
+        await db
+          .select({ ...getTableColumns(comment), user })
+          .from(comment)
+          .leftJoin(users, eq(comment.userId, users.id))
+          .where(eq(comment.id, newComment.id))
+      ).at(0)!;
+    },
+    { body: commentInsertSimpleSchema },
+  )
+  .delete(
+    "/:id",
+    async ({ params, request }) => {
+      const session = await getSession(request, authOpts);
+
+      if (!session?.user?.me?.id)
+        throw new Error("You must be logged in to delete a comment.");
+
+      return (
+        await db
+          .delete(comment)
+          .where(
+            and(
+              eq(comment.id, params.id),
+              eq(comment.userId, session?.user?.me?.id),
+            ),
+          )
+          .returning()
+      ).at(0);
+    },
+    { params: t.Pick(commentSelectSchema, ["id"]) },
+  );
