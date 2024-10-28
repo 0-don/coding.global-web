cec81778b3edb5aa0deb615dd8b63f6d1eab766c
0-don <don.cryptus@gmail.com>
2024-10-29 00:36:39 +0100

    easy

--- src/routes/api/[...path].ts
+++ src/routes/api/[...path].ts
@@ -1,6 +1,5 @@
 import { APIEvent } from "@solidjs/start/server";
-import { Elysia } from "elysia";
-import { commentRoute } from "~/server/comment/route";
+import { app } from "./elysia";
 
 const handler = async (event: APIEvent) => await app.handle(event.request);
 
@@ -9,7 +8,3 @@ export const POST = handler;
 export const PUT = handler;
 export const PATCH = handler;
 export const DELETE = handler;
-
-export const app = new Elysia({ prefix: "/api", aot: false }).use(commentRoute);
-
-export type App = typeof app;
