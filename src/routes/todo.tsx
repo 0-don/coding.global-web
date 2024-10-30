aeb82e2668081799aea6243432ebf9fe1c1ffc8c
0-don <don.cryptus@gmail.com>
2024-10-30 23:11:45 +0100

    easy

--- src/routes/todo.tsx
+++ src/routes/todo.tsx
@@ -1,10 +1,10 @@
 import { RouteDefinition } from "@solidjs/router";
-import { For, JSX, Show } from "solid-js";
+import { For, Show } from "solid-js";
 import { Layout } from "~/components/container/layout";
 import { prefetchTodos, TodoHook } from "~/components/hook/todo-hook";
 import { Button } from "~/components/ui/button";
 import { Card } from "~/components/ui/card";
-import { TextField } from "~/components/ui/textfield";
+import { TextField, TextFieldRoot } from "~/components/ui/textfield";
 import { todoSchemas } from "~/lib/schema/todo";
 
 export const route = {
@@ -14,7 +14,7 @@ export const route = {
 export default function Demo() {
   const { todo, setTodo, todoAdd, todosQuery, todoDelete } = TodoHook();
 
-  const onSubmit: JSX.IntrinsicElements["form"]["onsubmit"] = async (e) => {
+  const onSubmit = (e: SubmitEvent) => {
     e.preventDefault();
     if (!todoAdd.isPending && todoSchemas.insert.safeParse(todo).success)
       todoAdd.mutate();
@@ -47,12 +47,16 @@ export default function Demo() {
         </Show>
         <br />
         <form onSubmit={onSubmit} class={"flex flex-row justify-center gap-4"}>
-          <TextField
-            class={"rounded border-2 px-2 py-1"}
-            type={"text"}
-            value={todo.data}
-            onInput={({ currentTarget: { value: data } }) => setTodo({ data })}
-          />
+          <TextFieldRoot class="w-full max-w-xs" validationState="invalid">
+            <TextField
+              class={"rounded border-2 px-2 py-1"}
+              type={"text"}
+              value={todo.data}
+              onInput={({ currentTarget: { value: data } }) =>
+                setTodo({ data })
+              }
+            />
+          </TextFieldRoot>
           <Button
             disabled={
               todoAdd.isPending || !todoSchemas.insert.safeParse(todo).success
