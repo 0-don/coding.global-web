6f10bfd4351d8b6f312ae17926faeee0d5819f60
0-don <don.cryptus@gmail.com>
2024-10-30 23:55:52 +0100

    easy

--- src/components/hook/discord-hook.ts
+++ src/components/hook/discord-hook.ts
@@ -1,12 +1,12 @@
 import { cache } from "@solidjs/router";
 import { createQuery } from "@tanstack/solid-query";
 import { STAFF_MEMBERS_KEY } from "~/utils/cache/keys";
-import { clientEnv } from "~/utils/env/client";
+import { serverEnv } from "~/utils/env/server";
 import { Staff } from "~/utils/types";
 
 const prefetchStaffMembers = cache(async () => {
   "use server";
-  return (await fetch(clientEnv.STAFF_MEMERS_URL).then((res) =>
+  return (await fetch(serverEnv.STAFF_MEMERS_URL).then((res) =>
     res.json(),
   )) as Staff[];
 }, STAFF_MEMBERS_KEY);
