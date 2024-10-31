755f279440e0cc0867eb4eb6125344d9b7a7d445
0-don <don.cryptus@gmail.com>
2024-10-31 22:42:59 +0100

    easy

--- src/components/container/header.tsx
+++ src/components/container/header.tsx
@@ -2,12 +2,14 @@ import { A } from "@solidjs/router";
 import { HiSolidHomeModern } from "solid-icons/hi";
 import { Component } from "solid-js";
 import { Button } from "~/components/ui/button";
+import { useLanguage } from "../provider/language-provider";
 
 interface HeaderProps {
   name: string;
 }
 
 export const Header: Component<HeaderProps> = (props) => {
+  const { t, setLocale, locale } = useLanguage();
   return (
     <div class="grid grid-cols-3 content-center">
       <A href="/">
