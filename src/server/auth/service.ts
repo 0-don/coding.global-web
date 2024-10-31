c2efc8e3ad5aafb37d93deffe8e0d6b151037412
0-don <don.cryptus@gmail.com>
2024-10-31 17:33:32 +0100

    easy

--- src/server/auth/service.ts
+++ src/server/auth/service.ts
@@ -6,17 +6,15 @@ import { authOptions } from "./auth-options";
 export const authUserGuard = async ({ request }: Context<any, any>) => {
   const session = await getSession(request, authOptions);
 
-  if (!session) {
+  if (!session?.user)
     throw new InvalidCookieSignature(msg("MAIN.ERROR.UNAUTHORIZED"));
-  }
 };
 
 export const authUserResolve = async ({ request }: Context<any>) => {
   const session = await getSession(request, authOptions);
 
-  if (!session?.user) {
+  if (!session?.user)
     throw new InvalidCookieSignature(msg("MAIN.ERROR.UNAUTHORIZED"));
-  }
 
   return {
     user: session.user,
