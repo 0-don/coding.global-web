ae0426f45f676bc53e4230a0913977f43aec7fc3
0-don <don.cryptus@gmail.com>
2024-10-31 23:25:13 +0100

    easy

--- src/components/container/navbar/console-toggle.tsx
+++ src/components/container/navbar/console-toggle.tsx
@@ -1,22 +1,25 @@
 import { query } from "@solidjs/router";
 import { SiGooglesearchconsole } from "solid-icons/si";
 import { toast } from "solid-sonner";
+import { useLanguage } from "~/components/provider/language-provider";
 import { Button } from "~/components/ui/button";
+import { CONSOLE_KEY } from "~/utils/cache/keys";
 
 const clearConsole = query(() => {
   "use server";
   console.clear();
-}, "console");
+}, CONSOLE_KEY);
 
 export function ConsoleToggle() {
+  const { t } = useLanguage();
   return (
     <Button
-      title="Clear console"
+      title={t("MAIN.TOOLTIP.CLEAR_CONSOLE")}
       size="icon"
       onClick={() => {
         clearConsole();
         console.clear();
-        toast.success("Console cleared");
+        toast.success(t("MAIN.SUCCESS.CONSOLE_CLEARED"));
       }}
       variant="ghost"
     >
--- src/utils/cache/keys.ts
+++ src/utils/cache/keys.ts
@@ -1,3 +1,4 @@
 export const TODOS_KEY = "todos";
 export const COMMENTS_KEY = "comments";
 export const STAFF_MEMBERS_KEY = "staffMembers";
+export const CONSOLE_KEY = "console";
