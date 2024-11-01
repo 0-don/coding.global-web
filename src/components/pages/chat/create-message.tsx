5ca53b0d6372fbb1a6fd7d2c3af7e7f4b133aff3
0-don <don.cryptus@gmail.com>
2024-11-01 03:03:01 +0100

    easy

--- src/components/pages/chat/create-message.tsx
+++ src/components/pages/chat/create-message.tsx
@@ -1,10 +1,14 @@
 import { useAuth } from "@solid-mediakit/auth/client";
 import { FaBrandsDiscord } from "solid-icons/fa";
-import { JSX, Show, createSignal } from "solid-js";
+import { Show, createSignal } from "solid-js";
 import { CommentHook } from "~/components/hook/comment-hook";
 import { Button } from "~/components/ui/button";
 import { Grid } from "~/components/ui/grid";
-import { TextFieldLabel, TextField } from "~/components/ui/textfield";
+import {
+  TextField,
+  TextFieldLabel,
+  TextFieldRoot,
+} from "~/components/ui/textfield";
 
 interface CreateMessageProps {
   class?: string;
@@ -15,27 +19,29 @@ export default function CreateMessage(props: CreateMessageProps) {
   const [content, setContent] = createSignal("");
   const auth = useAuth();
 
-  const handleSubmit: JSX.IntrinsicElements["form"]["onsubmit"] = async (e) => {
+  const onSubmit = async (e: SubmitEvent) => {
     e.preventDefault();
     await commentAdd.mutateAsync({ content: content() });
     setContent("");
   };
 
   return (
-    <div class={props.class}>
+    <form onSubmit={onSubmit} class={props.class}>
       <Show when={auth.session()}>
         <Grid class="gap-1">
-          <TextFieldLabel class="sr-only" for="content">
-            Comment
-          </TextFieldLabel>
-          <TextField
-            class="bg-gray-200 focus-visible:ring-0 focus-visible:ring-offset-0 dark:bg-zinc-700"
-            value={content()}
-            onInput={(e) => setContent(e.currentTarget.value)}
-            id="content"
-            type="text"
-            placeholder="write your comment"
-          />
+          <TextFieldRoot class="w-full max-w-xs" validationState="invalid">
+            <TextFieldLabel class="sr-only" for="content">
+              Comment
+            </TextFieldLabel>
+            <TextField
+              class="bg-gray-200 focus-visible:ring-0 focus-visible:ring-offset-0 dark:bg-zinc-700"
+              value={content()}
+              onInput={(e) => setContent(e.currentTarget.value)}
+              id="content"
+              type="text"
+              placeholder="write your comment"
+            />
+          </TextFieldRoot>
         </Grid>
       </Show>
       <Show when={!auth.session()}>
@@ -47,11 +53,11 @@ export default function CreateMessage(props: CreateMessageProps) {
           class="w-full rounded-md bg-discord px-2 py-1 text-2xl font-black hover:bg-discord hover:opacity-90"
         >
           <div class="flex items-center text-white">
-            <FaBrandsDiscord />
+            <FaBrandsDiscord class="mr-1" />
             <span>Discord Login</span>
           </div>
         </Button>
       </Show>
-    </div>
+    </form>
   );
 }
--- src/routes/chat.tsx
+++ src/routes/chat.tsx
@@ -3,6 +3,8 @@ import { createEffect } from "solid-js";
 import { Header } from "~/components/container/header";
 import { CommentHook } from "~/components/hook/comment-hook";
 import { prefetchTodos } from "~/components/hook/todo-hook";
+import { ChatMessages } from "~/components/pages/chat/chat-messages";
+import CreateMessage from "~/components/pages/chat/create-message";
 import { Card } from "~/components/ui/card";
 import { Layout } from "../components/container/layout";
 
@@ -21,8 +23,8 @@ export default function Chat() {
     <Layout container class="mt-10 h-[calc(100vh-5rem)]">
       <Card class="flex h-full flex-col bg-secondary/85 p-10">
         <Header name="CHAT.TITLE" />
-        {/* <ChatMessages class="my-5 flex-1 overflow-y-auto" />
-        <CreateMessage /> */}
+        <ChatMessages class="my-5 flex-1 overflow-y-auto" />
+        <CreateMessage />
       </Card>
     </Layout>
   );
