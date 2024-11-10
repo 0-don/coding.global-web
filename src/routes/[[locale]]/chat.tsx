69c245825d60e7ba183c6e154904141812cdbb8f
0-don <don.cryptus@gmail.com>
2024-11-10 03:39:38 +0100

    easy

--- src/routes/[[locale]]/chat.tsx
+++ src/routes/[[locale]]/chat.tsx
@@ -1,9 +1,11 @@
 import { RouteDefinition } from "@solidjs/router";
 import { Header } from "~/components/container/header";
 import { Layout } from "~/components/container/layout";
+import { MetaHead } from "~/components/elements/meta-head";
 import { serverFnComments } from "~/components/hook/chat-hook";
 import { ChatMessages } from "~/components/pages/chat/chat-messages";
 import CreateMessage from "~/components/pages/chat/create-message";
+import { useLanguage } from "~/components/provider/language-provider";
 import { Card } from "~/components/ui/card";
 
 export const route = {
@@ -11,13 +13,21 @@ export const route = {
 } satisfies RouteDefinition;
 
 export default function ChatPage() {
+  const { t } = useLanguage();
   return (
-    <Layout container class="mt-10 h-[calc(100vh-5rem)]">
-      <Card class="flex h-full flex-col bg-secondary/85 p-10">
-        <Header name="CHAT.TITLE" />
-        <ChatMessages class="my-5 flex-1 overflow-y-auto" />
-        <CreateMessage />
-      </Card>
-    </Layout>
+    <>
+      <MetaHead
+        title={t("HOME.META.TITLE")!}
+        description={t("HOME.META.DESCRIPTION")!}
+        keywords={t("HOME.META.KEYWORDS")}
+      />
+      <Layout container class="mt-10 h-[calc(100vh-5rem)]">
+        <Card class="flex h-full flex-col bg-secondary/85 p-10">
+          <Header name="CHAT.TITLE" />
+          <ChatMessages class="my-5 flex-1 overflow-y-auto" />
+          <CreateMessage />
+        </Card>
+      </Layout>
+    </>
   );
 }
