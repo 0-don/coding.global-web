35f6d6ee62b20dabf2aaf8c2a187d13849d13888
0-don <don.cryptus@gmail.com>
2024-11-10 17:35:56 +0100

    easy

--- src/lib/schema/index.ts
+++ src/lib/schema/index.ts
@@ -1,3 +1,3 @@
 export { accounts, sessions, users, verificationTokens } from "./auth";
 export { comment } from "./comment";
-export { todo } from "./todo";
+export { todo, todoStatus } from "./todo";
--- src/lib/schema/todo.ts
+++ src/lib/schema/todo.ts
@@ -1,17 +1,22 @@
-import { pgTable, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
+import { pgEnum, pgTable, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
 import { createSelectSchema } from "drizzle-typebox";
 import { Elysia, t } from "elysia";
+
+export const todoStatus = pgEnum("todoStatus", ["DONE", "ACTIVE", "PENDING"]);
 
 export const todo = pgTable("todo", {
   id: uuid("id").primaryKey().defaultRandom(),
   task: varchar("task", { length: 4096 }).notNull(),
+  status: todoStatus("status").default("PENDING").notNull(),
   createdAt: timestamp("createdAt").defaultNow(),
 });
 
 export const todoSelectSchema = createSelectSchema(todo, {
   task: t.String({ minLength: 1, maxLength: 4096, default: "" }),
 });
+
 export type Todo = typeof todoSelectSchema.static;
+export type TodoStatus = (typeof todoStatus.enumValues)[number];
 
 export const todoInsertSchema = t.Omit(todoSelectSchema, ["id", "createdAt"]);
 export const todoInsertSeedSchema = t.Omit(todoSelectSchema, ["id"]);
