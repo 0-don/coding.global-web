d8fcdfaecc5af639e7373280538902fd41206c8b
0-don <don.cryptus@gmail.com>
2024-11-10 18:22:17 +0100

    easy

---
+++ src/components/elements/data-table/tasks.ts
@@ -0,0 +1,26 @@
+export type Task = {
+  id: string;
+  code: string;
+  title: string;
+  status: "todo" | "in-progress" | "done" | "cancelled";
+  label: "bug" | "feature" | "enhancement" | "documentation";
+};
+
+export const tasks: Task[] = [
+  {
+    id: "ptL0KpX_yRMI98JFr6B3n",
+    code: "TASK-33",
+    title: "We need to bypass the redundant AI interface!",
+    status: "todo",
+    label: "bug",
+  },
+  {
+    id: "RsrTg_SmBKPKwbUlr7Ztv",
+    code: "TASK-59",
+    title:
+      "Overriding the capacitor won't do anything, we need to generate the solid state JBOD pixel!",
+    status: "in-progress",
+    label: "feature",
+  },
+  // ...
+];
