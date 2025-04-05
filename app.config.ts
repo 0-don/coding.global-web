0b53508cd02646b7fb04ec7a645a43db9819268e
0-don <70039285+0-don@users.noreply.github.com>
2025-04-05 23:07:24 +0200

    easy

--- app.config.ts
+++ app.config.ts
@@ -1,4 +1,3 @@
-import { authVite } from "@solid-mediakit/auth-plugin";
 import { defineConfig } from "@solidjs/start/config";
 import { resolve } from "node:path";
 import { solidStartSiteMapPlugin } from "solid-start-sitemap";
@@ -6,26 +5,11 @@ import { solidStartSiteMapPlugin } from "solid-start-sitemap";
 export default defineConfig({
   ssr: true,
   middleware: "./src/server/middleware.ts",
-  server: {
-    esbuild: {
-      options: {
-        target: "esnext",
-      },
-    },
-  },
   vite: {
     resolve: {
       alias: { "@": resolve("./src") },
     },
     plugins: [
-      authVite({
-        redirectTo: "/",
-        log: true,
-        authOpts: {
-          name: "authOptions",
-          dir: "~/server/auth-options",
-        },
-      }),
       solidStartSiteMapPlugin({
         hostname: process.env.VITE_HOST_URL,
         replaceRouteParams: {
