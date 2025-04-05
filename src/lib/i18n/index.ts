ee2057eca121ba0b8d34ac23006bfc7c3fe89e9a
0-don <70039285+0-don@users.noreply.github.com>
2025-04-06 00:05:02 +0200

    easy

--- src/lib/i18n/index.ts
+++ src/lib/i18n/index.ts
@@ -1,14 +1,14 @@
 import { type Flatten, flatten } from "@solid-primitives/i18n";
-import { clientEnv } from "~/utils/env/client";
 import type * as de from "./de.json";
 
 export type RawDictionary = typeof de;
-export type Locale = (typeof clientEnv)["LANGUAGES"][number];
+export type Locale = (typeof LANGUAGES)[number];
 export type Dictionary = Flatten<RawDictionary>;
 export type DictionaryKey = {
   [K in keyof Dictionary]: Dictionary[K] extends string ? K : never;
 }[keyof Dictionary];
 
+export const LANGUAGES = ["en", "de"] as const;
 
 export async function fetchDictionary(locale: Locale): Promise<Dictionary> {
   const dict: RawDictionary = await import(`./${locale}.json?import`);
--- src/utils/env/client/index.ts
+++ src/utils/env/client/index.ts
@@ -1,50 +1,51 @@
-import { Elysia, t } from "elysia";
+import { Type as t } from "@sinclair/typebox";
+import { TypeCompiler } from "@sinclair/typebox/compiler";
+import { LANGUAGES, Locale } from "~/lib/i18n";
+import { safeParse } from "~/utils/base";
 
-const {
-  models: { clientSchema },
-} = new Elysia().model({
-  clientSchema: t.Object({
-    HOST_URL: t.String({
-      minLength: 1,
-      error: "VITE_HOST_URL client environment variable is not set!",
-    }),
-    BOT_URL: t.String({
-      minLength: 1,
-      error: "BOT_URL client environment variable is not set!",
-    }),
-    GUILD_ID: t.String({
-      minLength: 1,
-      error: "GUILD_ID client environment variable is not set!",
-    }),
-    LANGUAGE_KEY: t.Union([t.Literal("lang"), t.String()], {
-      error: "LANGUAGE_KEY client environment variable is not set!",
-    }),
-    LANGUAGES: t.Array(t.Union([t.Literal("de"), t.Literal("en")]), {
-      minLength: 3,
-      error: "LANGUAGES client environment variable is not set!",
-    }),
+const clientSchema = t.Object({
+  HOST_URL: t.String({
+    minLength: 1,
+    error: "VITE_HOST_URL client environment variable is not set!",
+  }),
+  BOT_URL: t.String({
+    minLength: 1,
+    error: "BOT_URL client environment variable is not set!",
+  }),
+  GUILD_ID: t.String({
+    minLength: 1,
+    error: "GUILD_ID client environment variable is not set!",
+  }),
+  LANGUAGE_KEY: t.Union([t.Literal("lang"), t.String()], {
+    error: "LANGUAGE_KEY client environment variable is not set!",
+  }),
+  LANGUAGES: t.Array(t.Union([t.Literal("de"), t.Literal("en")]), {
+    minLength: 3,
+    error: "LANGUAGES client environment variable is not set!",
   }),
 });
+const clientSchemaChecker = TypeCompiler.Compile(clientSchema);
 
-const clientEnvResult = clientSchema.safeParse({
+const clientEnvResult = safeParse(clientSchemaChecker, {
   HOST_URL:
     import.meta.env.VITE_HOST_URL ??
     `http://localhost:${import.meta.env.VITE_PORT ?? 3000}`,
   BOT_URL: import.meta.env.VITE_BOT_URL ?? "https://bot.coding.global",
   GUILD_ID: import.meta.env.VITE_GUILD_ID ?? "693908458986143824",
   LANGUAGE_KEY: import.meta.env.VITE_LANGUAGE_KEY ?? "lang",
-  LANGUAGES: import.meta.env.VITE_LANGUAGES
+  LANGUAGES: (import.meta.env.VITE_LANGUAGES
     ? import.meta.env.VITE_LANGUAGES.split(",")
-    : ["en", "de"],
+    : LANGUAGES) as Locale[],
 });
 
-if (!clientEnvResult.data) {
+if (!clientEnvResult.success) {
   const firstError = clientEnvResult.errors[0];
-  if (firstError)
+  if (firstError) {
     throw new Error(
-      `Invalid client environment variable ${firstError.path.slice(1)}: ${firstError.summary.replaceAll("  ", " ")}`,
+      `Invalid client environment variable: ${firstError.message}`,
     );
-  else throw new Error(`Invalid client environment ${clientEnvResult.error}`);
+  }
+  throw new Error(`Invalid client environment validation failed`);
 }
 
 export const clientEnv = clientEnvResult.data;
