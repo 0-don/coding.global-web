acfa232d3d781ae2449c00afbbb394780a71d36a
0-don <don.cryptus@gmail.com>
2025-08-17 20:47:32 +0200

    fix: clean up imports and improve error handling in API routes chore: update .gitignore to exclude environment files feat: add .env configuration for environment variables

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,22 +1,17 @@
-import { Elysia, t } from "elysia";
+import { Elysia } from "elysia";
 
 const API_URL = process.env.CODING_API_URL;
 const SERVER = process.env.SERVER_ID;
 
-if (!API_URL) {
-  throw new Error("CODING_API_URL environment variable is missing");
-}
-if (!SERVER) {
-  throw new Error("SERVER_ID environment variable is missing");
-}
- 
-export const api = new Elysia({prefix:'/api'})
+const app = new Elysia({ prefix: "/api" })
   .get("/team", async () => {
     try {
       const response = await fetch(`${API_URL}api/${SERVER}/staff`);
 
       if (!response.ok) {
-        throw new Error(`Failed to fetch staff data: ${response.status} ${response.statusText}`);
+        throw new Error(
+          `Failed to fetch staff data: ${response.status} ${response.statusText}`
+        );
       }
 
       const data = await response.json();
@@ -26,12 +21,14 @@ export const api = new Elysia({prefix:'/api'})
       return { error: "Unable to fetch team data" };
     }
   })
-    .get("/news", async () => {
+  .get("/news", async () => {
     try {
       const response = await fetch(`${API_URL}api/${SERVER}/news`);
 
       if (!response.ok) {
-        throw new Error(`Failed to fetch data: ${response.status} ${response.statusText}`);
+        throw new Error(
+          `Failed to fetch data: ${response.status} ${response.statusText}`
+        );
       }
 
       const data = await response.json();
@@ -40,10 +37,12 @@ export const api = new Elysia({prefix:'/api'})
       console.error("Error fetching data:", error);
       return { error: "Unable to fetch data" };
     }
-  })
- 
-export type Bknd = typeof api;
+  });
 
-export const GET = api.handle;
-export const POST = api.handle
+export type App = typeof app;
 
+export const GET = app.handle;
+export const POST = app.handle;
+export const PUT = app.handle;
+export const DELETE = app.handle;
+export const PATCH = app.handle;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,15 @@
-import Background from "./components/Background";
-import Hero from "./components/hero/Hero";
-import Footer from "./components/footer";
-import Navbar from "./components/NavBar";
+import Background from "@/components/Background";
+import Footer from "@/components/footer";
+import Hero from "@/components/hero/Hero";
+import Navbar from "@/components/NavBar";
 
 export default function Main() {
   return (
     <div className="relative w-full h-screen ...">
       <Background />
       <Navbar />
-      <Hero />   
+      <Hero />
       <Footer />
     </div>
   );
 }
-
- 
- 
