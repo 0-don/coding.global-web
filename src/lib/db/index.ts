cd2c033dac296f48c35c802d0b5ff144ed347e20
0-don <don.cryptus@gmail.com>
2025-12-05 00:47:24 +0100

    Refactor database schema: consolidate auth, comment, and todo schemas into dedicated files; update index exports for improved organization

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,9 +1,9 @@
 import { getTableColumns, sql } from "drizzle-orm";
 import { PgTable } from "drizzle-orm/pg-core";
 
-export { account, session, user, verification } from "./auth-schema";
-export { comment } from "./comment-schema";
-export { todo, todoStatus } from "./todo-schema";
+export { account, session, user, verification } from "./auth-db-schema";
+export { comment } from "./comment-db-schema";
+export { todo, todoStatus } from "./todo-db-schema";
 
 export function conflictUpdateAllExcept<
   T extends PgTable,
