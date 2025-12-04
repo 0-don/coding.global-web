780c822eb5ff978454a91c64eaeb1e62ff74d345
0-don <don.cryptus@gmail.com>
2025-12-04 22:19:46 +0100

    Add robots.txt to allow all user agents and specify sitemap location

--- old/src/lib/schema/index.ts
+++
@@ -1,3 +0,0 @@
-export { accounts, sessions, users, verificationTokens } from "./auth";
-export { comment } from "./comment";
-export { todo, todoStatus } from "./todo";
--- old/src/routes/api/auth/[...solidauth].js
+++
@@ -1,3 +0,0 @@
-import { SolidAuth } from "@solid-mediakit/auth";
-import { authOptions } from "~/server/auth/auth-options";
-export const { GET, POST } = SolidAuth(authOptions);
