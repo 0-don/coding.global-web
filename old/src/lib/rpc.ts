f1d8b5ec5646ed56faca27f9b9bed4f56108ece3
0-don <don.cryptus@gmail.com>
2025-12-04 22:19:46 +0100

    Add robots.txt to allow all user agents and specify sitemap location

--- old/src/lib/rpc.ts
+++
@@ -1,8 +0,0 @@
-import { edenTreaty } from "@elysiajs/eden";
-import type { App } from "../server/server";
-
-export const rpc = edenTreaty<App>(
-  typeof window === "undefined"
-    ? `http://localhost:${process.env.PORT ?? 3000}`
-    : window.location.origin,
-);
--- old/src/server/server.ts
+++
@@ -1,9 +0,0 @@
-import { Elysia } from "elysia";
-import { commentRoute } from "~/server/comment/route";
-import { todoRoute } from "./todo/route";
-
-export const app = new Elysia({ prefix: "/api", aot: false })
-  .use(todoRoute)
-  .use(commentRoute);
-
-export type App = typeof app;
