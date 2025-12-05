d3ef6a465c73de5f2202da1ff384b8e9e52204cd
0-don <don.cryptus@gmail.com>
2025-12-05 23:52:56 +0100

    Refactor environment variables and constants: update NEXT_PUBLIC_HOST_URL to NEXT_PUBLIC_URL, add NEXT_PUBLIC_APP_NAME, and reorganize constants for better structure; add internationalization support with new language files for English and German.

---
+++ src/lib/config/metadata.ts
@@ -0,0 +1,78 @@
+import { Metadata } from "next";
+import { Locale } from "next-intl";
+import { ALTERNATE_LANGUAGES, LANGUAGES, LOCALES } from "./constants";
+
+type Params = {
+  locale: Locale | (string & {});
+  title: string;
+  description: string;
+  keywords: string;
+  path?: string;
+  ogImage?: string;
+  robots?: boolean;
+};
+
+export async function getPageMetadata(params: Params): Promise<Metadata> {
+  const canonicalPath = params.path || `/${params.locale}`;
+  const ogImageUrl = params.ogImage || "/images/logo-white-open-graph.png";
+  const shouldIndex = params.robots ?? true;
+
+  return {
+    metadataBase: new URL(process.env.NEXT_PUBLIC_URL),
+    title: params.title,
+    description: params.description,
+    keywords: params.keywords.split(", "),
+    authors: [{ name: process.env.NEXT_PUBLIC_APP_NAME }],
+    alternates: {
+      canonical: canonicalPath,
+      languages: {
+        ...ALTERNATE_LANGUAGES,
+        "x-default": `/${LOCALES[0]}`,
+      },
+    },
+    openGraph: {
+      title: params.title,
+      description: params.description,
+      type: "website",
+      locale: LANGUAGES.find((l) => l.code === params.locale.toUpperCase())
+        ?.ogLocale,
+      alternateLocale: LANGUAGES.map((l) => l.ogLocale),
+      siteName: process.env.NEXT_PUBLIC_APP_NAME,
+      images: [
+        {
+          url: ogImageUrl,
+          alt: params.title,
+          type: "image/png",
+          width: 1200,
+          height: 628,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: params.title,
+      description: params.description,
+      images: [
+        {
+          url: ogImageUrl,
+          alt: params.title,
+          type: "image/png",
+          width: 1200,
+          height: 628,
+        },
+      ],
+    },
+    robots: {
+      index: shouldIndex,
+      follow: shouldIndex,
+      nocache: !shouldIndex,
+      googleBot: {
+        index: shouldIndex,
+        follow: shouldIndex,
+        "max-video-preview": -1,
+        "max-image-preview": "large",
+        "max-snippet": -1,
+      },
+    },
+  };
+}
