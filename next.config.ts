99061c237b6ad4f902f874c0be44d7dd42ee7138
0-don <don.cryptus@gmail.com>
2025-12-06 18:41:21 +0100

    Refactor Dockerfile and update internationalization files: streamline build process, set environment variables, and enhance language support with new action and manifest entries in English and German.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: process.env.STANDALONE ? "standalone" : undefined,
   images: {
     qualities: [75, 100],
   },
