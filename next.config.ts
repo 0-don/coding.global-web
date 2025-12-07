f89bdbe31a7a1222f2f978b8e27a3ace7f7e04c9
0-don <don.cryptus@gmail.com>
2025-12-07 21:18:44 +0100

    fix: update output configuration in next.config.ts to support environment variable

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
+  output: process.env.STANDALONE ? "standalone" : undefined,
   // trailingSlash: true,
 
   images: {
