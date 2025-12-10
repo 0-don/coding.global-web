ff666b7bffbf4dfb2959da48bc2035c537be74e6
0-don <don.cryptus@gmail.com>
2025-12-10 23:23:54 +0100

    feat: update footer component to include dynamic app name and repository link with translations

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,18 +1,32 @@
+"use client";
+
+import Link from "next/link";
+import { useTranslations } from "next-intl";
+
 export function Footer() {
+  const t = useTranslations();
+
   return (
     <footer className="fixed right-0 bottom-0 left-0 z-10 flex items-center justify-center gap-5 py-4 text-center">
       <code className="text-sm text-gray-400">
-        Coding Global 2025 &copy; Tokyo
+        {process.env.NEXT_PUBLIC_APP_NAME} {new Date().getFullYear()} &copy;{" "}
+        <Link
+          href="https://github.com/0-don"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          0-don
+        </Link>
       </code>
       <p className="text-sm text-gray-400">|</p>
-      <a
+      <Link
         href="https://github.com/0-don/coding.global-web"
         className="text-sm text-gray-400"
         target="_blank"
         rel="noopener noreferrer"
       >
-        Get the repository
-      </a>
+        {t("MAIN.FOOTER.GET_REPOSITORY")}
+      </Link>
     </footer>
   );
 }
