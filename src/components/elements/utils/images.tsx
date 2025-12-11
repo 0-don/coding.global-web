aa30533a46fbcefb0ede13a4c7f68b2b6597a93a
0-don <don.cryptus@gmail.com>
2025-12-11 22:08:54 +0100

    feat: add mobile navigation and navbar components with logo and company name
    - Implemented `LogoImage` and `CompanyName` components for branding.
    - Created `MobileNav` component for responsive navigation with language and theme toggles.
    - Developed `Navbar` component with navigation links and user authentication buttons.
    - Added utility functions for navigation item management and active link detection.
    - Introduced user avatar and dropdown for user actions and logout functionality.
    - Integrated Radix UI components for navigation and sheet functionalities.

---
+++ src/components/elements/utils/images.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import NextImage from "next/image";
+
+export const LogoImage = (
+  props: Omit<React.ComponentProps<typeof NextImage>, "src" | "alt">,
+) => {
+  const appName = process.env.NEXT_PUBLIC_APP_NAME || "Trade Reclaim";
+  return (
+    <NextImage
+      height={20}
+      width={20}
+      {...props}
+      src={"/images/cgLogo.gif"}
+      alt={appName}
+      className="h-auto w-auto"
+    />
+  );
+};
+
+export const CompanyName = (props: { className?: string }) => {
+  const appName = process.env.NEXT_PUBLIC_APP_NAME || "Trade Reclaim";
+  const firstWord = appName.split(" ")[0];
+  const secondWord = appName.split(" ")[1] || "";
+  return (
+    <div className={cn("flex gap-1", props.className)}>
+      <span>{firstWord}</span>
+      <span className="text-primary">{secondWord}</span>
+    </div>
+  );
+};
---
+++ src/components/layout/user/user-dropdown.tsx
@@ -0,0 +1,108 @@
+"use client";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useSessionHook } from "@/hook/session-hook";
+import { authClient } from "@/lib/auth-client";
+import { deleteCookie, getCookies } from "cookies-next/client";
+import { useTranslations } from "next-intl";
+import { ReactNode } from "react";
+import { IoLogOut } from "react-icons/io5";
+
+interface UserDropdownProps {
+  children: ReactNode;
+  side?: "top" | "bottom" | "left" | "right";
+  align?: "start" | "center" | "end";
+  sideOffset?: number;
+  className?: string;
+}
+
+export function UserDropdown({
+  children,
+  side = "bottom",
+  align = "end",
+  sideOffset = 4,
+  className,
+}: UserDropdownProps) {
+  const session = useSessionHook();
+  const t = useTranslations();
+
+  if (!session?.data?.user.id) {
+    return null;
+  }
+
+  const handleLogout = async () => {
+    // Clear all cookies
+    Object.keys(getCookies() ?? {}).forEach((cookieName) =>
+      deleteCookie(cookieName),
+    );
+
+    await authClient.signOut();
+    setTimeout(() => window.location.reload()); // react-query hydration issue workaround
+  };
+
+  const username = session.data.user.name || "";
+  const name = username?.split("@")[0] || "";
+  const firstEmailChar = name.charAt(0).toUpperCase();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild className={className}>
+        {children}
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        className="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
+        side={side}
+        align={align}
+        sideOffset={sideOffset}
+      >
+        <DropdownMenuLabel className="p-0 font-normal">
+          <div className="flex flex-col gap-2 px-1 py-1.5 text-left text-sm">
+            <div className="flex items-center gap-2">
+              <Avatar className="h-8 w-8 rounded-lg">
+                <AvatarImage src={""} alt={name || ""} />
+                <AvatarFallback className="rounded-lg">
+                  {firstEmailChar}
+                </AvatarFallback>
+              </Avatar>
+              <div className="grid flex-1 text-left text-sm leading-tight">
+                <span className="truncate font-medium">{name}</span>
+                <span className="text-muted-foreground truncate text-xs">
+                  {username}
+                </span>
+              </div>
+            </div>
+            {/* {me?.data?.data.roles && me.data.data.roles.length && (
+              <div className="flex flex-wrap gap-1 pt-1">
+                {me?.data?.data.roles.map((role) => (
+                  <Badge key={role} variant="secondary" className="text-xs">
+                    {role}
+                  </Badge>
+                ))}
+              </div>
+            )} */}
+          </div>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        {/* <DropdownMenuGroup>
+          <DropdownMenuItem>
+            <IconUserCircle />
+            {t("MAIN.SIDEBAR.USER.SETTINGS")}
+          </DropdownMenuItem>
+        </DropdownMenuGroup> */}
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={handleLogout}>
+          <IoLogOut />
+          {t("MAIN.AUTH.LOGOUT")}
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
