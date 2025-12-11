80c2688eb967d500580cb5a7886d169ffed630c6
0-don <don.cryptus@gmail.com>
2025-12-12 00:12:10 +0100

    feat: add language options to localization files and update language toggle component

--- src/components/toggles/language-toggle.tsx
+++ src/components/toggles/language-toggle.tsx
@@ -50,7 +50,6 @@ export function LanguageToggle() {
             className="hover:bg-red-500 hover:text-black"
           >
             <span className="mr-2 text-lg">{lang.flag}</span>
-            {/* @ts-expect-error - Dynamic translation key */}
             {t(`MAIN.ENUM.${lang.code}`)}
           </DropdownMenuItem>
         ))}
