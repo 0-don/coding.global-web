03f871c7a71596775dcf40b58de73cc95e2af1a3
0-don <don.cryptus@gmail.com>
2025-12-14 00:36:31 +0100

    refactor: remove unnecessary text color classes and simplify Button component styling

--- src/components/pages/home.tsx
+++ src/components/pages/home.tsx
@@ -11,7 +11,7 @@ export function Home() {
   return (
     <div className="container mx-auto mt-10 flex h-full items-center gap-10 px-4 md:px-6">
       <div className="flex-1">
-        <Card className="border-primary text-primary mb-6 font-mono text-sm">
+        <Card className="border-primary mb-6 font-mono text-sm">
           <CardContent className="space-y-4">
             <div className="flex items-center space-x-2">
               <span className="bg-primary h-3 w-3 rounded-full"></span>
@@ -21,25 +21,20 @@ export function Home() {
 
             <div className="flex flex-wrap items-center justify-center space-x-1">
               <span className="text-green-400">{">"}</span>
-              <span className="ml-1 text-gray-300">{t("HOME.WELCOME_TO")}</span>
+              <span className="ml-1">{t("HOME.WELCOME_TO")}</span>
               <span className="text-primary font-bold">
                 {process.env.NEXT_PUBLIC_APP_NAME} {t("HOME.WEBSITE")}
               </span>
             </div>
 
-            <div className="animate-pulse text-center text-xs text-gray-400">
+            <div className="animate-pulse text-center text-xs">
               {t("HOME.WAITING_MESSAGE")}
             </div>
           </CardContent>
         </Card>
 
         <div className="z-10 flex items-center justify-center">
-          <Button
-            asChild
-            variant="outline"
-            size="lg"
-            className="border-primary hover:bg-primary hover:text-primary-foreground rounded-full border-2 px-8 py-3 font-bold shadow-lg transition-all duration-300 hover:scale-105"
-          >
+          <Button asChild variant="outline" size="lg">
             <Link href={getDiscordInviteLink()}>{t("HOME.JOIN_BUTTON")}</Link>
           </Button>
         </div>
