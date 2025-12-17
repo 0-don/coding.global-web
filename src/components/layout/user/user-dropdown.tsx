13521d808f3b86ab7c26736b480a7f4f9c40478a
0-don <don.cryptus@gmail.com>
2025-12-17 20:57:07 +0100

    fix: update avatar alt text to use username instead of display name in Discord components

--- src/components/layout/user/user-dropdown.tsx
+++ src/components/layout/user/user-dropdown.tsx
@@ -67,7 +67,7 @@ export function UserDropdown({
           <div className="flex flex-col gap-2 px-1 py-1.5 text-left text-sm">
             <div className="flex items-center gap-2">
               <Avatar className="h-8 w-8 rounded-lg">
-                <AvatarImage src={""} alt={name || ""} />
+                <AvatarImage src={""} alt={name} />
                 <AvatarFallback className="rounded-lg">
                   {firstEmailChar}
                 </AvatarFallback>
