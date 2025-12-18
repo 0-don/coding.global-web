c40e885c8c670d1471b35a5f9db925a29105da0f
0-don <don.cryptus@gmail.com>
2025-12-18 23:57:29 +0100

    feat: move DiscordWidget and WelcomeWidget components to their own files for better organization

--- src/components/pages/home/home.tsx
+++ src/components/pages/home/home.tsx
@@ -1,46 +1,11 @@
-import { DiscordWidget } from "@/components/elements/discord/discord-widget";
-import { Button } from "@/components/ui/button";
-import { Card, CardContent } from "@/components/ui/card";
-import { getDiscordInviteLink } from "@/lib/utils/base";
-import { useTranslations } from "next-intl";
-import Link from "next/link";
+import { DiscordWidget } from "@/components/pages/home/discord-widget";
+import { WelcomeWidget } from "./welcome-widget";
 
 export function Home() {
-  const t = useTranslations();
-
   return (
-    <div className="ga-5 container mx-auto mt-10 flex h-full flex-col items-center gap-10 px-4 md:flex-row md:px-6">
-      <div className="flex-1">
-        <Card className="border-primary mb-6 font-mono text-sm">
-          <CardContent className="space-y-4">
-            <div className="flex items-center space-x-2">
-              <span className="bg-primary h-3 w-3 rounded-full"></span>
-              <span className="h-3 w-3 rounded-full bg-yellow-500"></span>
-              <span className="h-3 w-3 rounded-full bg-green-500"></span>
-            </div>
+    <div className="container mx-auto mt-10 flex h-full flex-col items-center gap-10 px-4 md:flex-row md:px-6">
+      <WelcomeWidget className="flex-1" />
 
-            <div className="flex flex-wrap items-center justify-center space-x-1">
-              <span className="text-green-400">{">"}</span>
-              <span className="ml-1">{t("HOME.WELCOME_TO")}</span>
-              <span className="text-primary font-bold">
-                {process.env.NEXT_PUBLIC_APP_NAME} {t("HOME.WEBSITE")}
-              </span>
-            </div>
-
-            <div className="animate-pulse text-center text-xs">
-              {t("HOME.WAITING_MESSAGE")}
-            </div>
-          </CardContent>
-        </Card>
-
-        <div className="z-10 flex items-center justify-center">
-          <Button variant="outline" size="lg">
-            <Link href={getDiscordInviteLink()}>{t("HOME.JOIN_BUTTON")}</Link>
-          </Button>
-        </div>
-      </div>
-
-      {/* Discord Widget */}
       <DiscordWidget className="flex-1" />
     </div>
   );
---
+++ src/components/pages/home/welcome-widget.tsx
@@ -0,0 +1,47 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+import { getDiscordInviteLink } from "@/lib/utils/base";
+import { useTranslations } from "next-intl";
+import Link from "next/link";
+import React from "react";
+
+interface WelcomeWidgetProps {
+  className?: string;
+}
+
+export const WelcomeWidget: React.FC<WelcomeWidgetProps> = (props) => {
+  const t = useTranslations();
+
+  return (
+    <div className={cn(props.className)}>
+      <Card className="border-primary mb-6 font-mono text-sm">
+        <CardContent className="space-y-4">
+          <div className="flex items-center space-x-2">
+            <span className="bg-primary h-3 w-3 rounded-full"></span>
+            <span className="h-3 w-3 rounded-full bg-yellow-500"></span>
+            <span className="h-3 w-3 rounded-full bg-green-500"></span>
+          </div>
+
+          <div className="flex flex-wrap items-center justify-center space-x-1">
+            <span className="text-green-400">{">"}</span>
+            <span className="ml-1">{t("HOME.WELCOME_TO")}</span>
+            <span className="text-primary font-bold">
+              {process.env.NEXT_PUBLIC_APP_NAME} {t("HOME.WEBSITE")}
+            </span>
+          </div>
+
+          <div className="animate-pulse text-center text-xs">
+            {t("HOME.WAITING_MESSAGE")}
+          </div>
+        </CardContent>
+      </Card>
+
+      <div className="z-10 flex items-center justify-center">
+        <Button variant="outline" size="lg">
+          <Link href={getDiscordInviteLink()}>{t("HOME.JOIN_BUTTON")}</Link>
+        </Button>
+      </div>
+    </div>
+  );
+};
