c250555a8d65ed233e62678fadb816660834e090
0-don <don.cryptus@gmail.com>
2025-12-19 01:06:27 +0100

    feat: refactor showcase components by consolidating ShowcaseList and removing unused files

--- src/components/pages/showcase/showcase.tsx
+++ src/components/pages/showcase/showcase.tsx
@@ -19,8 +19,6 @@ export function ShowcaseList() {
   const t = useTranslations();
   const showcaseThreadsQuery = useShowcaseThreadsQuery();
 
-  console.log(showcaseThreadsQuery.data?.length);
-
   return (
     <div className="container mx-auto px-4 md:px-6">
       <div className="flex items-center justify-center gap-2 p-6">
