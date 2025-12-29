7e8c1be27fcb5c00dd4b01206a1192002834e49e
0-don <don.cryptus@gmail.com>
2025-12-29 22:33:43 +0100

    feat: rename SiteHeader to SidebarHeader and adjust imports in layout

--- src/components/layout/sidebar/sidebar-header.tsx
+++ src/components/layout/sidebar/sidebar-header.tsx
@@ -4,8 +4,7 @@ import { Separator } from "@/components/ui/separator";
 import { SidebarTrigger } from "@/components/ui/sidebar";
 import { SidebarSearch } from "./sidebar-search";
 
-export function SiteHeader() {
-
+export function SidebarHeader() {
   return (
     <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
       <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
