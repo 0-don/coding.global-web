9a69ed2e6fb5017e501a72cbba047ef4bbad1703
0-don <don.cryptus@gmail.com>
2026-01-01 01:50:09 +0100

    feat: add metadata for resources and related pages in multiple languages

--- src/app/[locale]/(sidebar)/resources/guides/cyber-security/page.tsx
+++ src/app/[locale]/(sidebar)/resources/guides/cyber-security/page.tsx
@@ -3,6 +3,22 @@ import {
   CyberSecurity,
   cyberSecurityTOC,
 } from "@/components/pages/resources/guides/cyber-security";
+import { getPageMetadata } from "@/lib/config/metadata";
+import { serverLocale } from "@/lib/utils/server";
+import { getTranslations } from "next-intl/server";
+
+export async function generateMetadata(props: {
+  params: Promise<{ locale: string }>;
+}) {
+  const locale = await serverLocale(props);
+  const t = await getTranslations({ locale });
+  return getPageMetadata({
+    locale,
+    title: t("RESOURCES.CYBER_SECURITY.META.TITLE"),
+    description: t("RESOURCES.CYBER_SECURITY.META.DESCRIPTION"),
+    keywords: t("RESOURCES.CYBER_SECURITY.META.KEYWORDS"),
+  });
+}
 
 export default function CyberSecurityPage() {
   return (
--- src/app/[locale]/(sidebar)/resources/page.tsx
+++ src/app/[locale]/(sidebar)/resources/page.tsx
@@ -1,5 +1,21 @@
 import { TOCLayout } from "@/components/layout/resources/toc";
 import { Resources, resourcesTOC } from "@/components/pages/resources/resources";
+import { getPageMetadata } from "@/lib/config/metadata";
+import { serverLocale } from "@/lib/utils/server";
+import { getTranslations } from "next-intl/server";
+
+export async function generateMetadata(props: {
+  params: Promise<{ locale: string }>;
+}) {
+  const locale = await serverLocale(props);
+  const t = await getTranslations({ locale });
+  return getPageMetadata({
+    locale,
+    title: t("RESOURCES.META.TITLE"),
+    description: t("RESOURCES.META.DESCRIPTION"),
+    keywords: t("RESOURCES.META.KEYWORDS"),
+  });
+}
 
 export default function ResourcesPage() {
   return (
