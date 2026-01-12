8a394d0175579603abbf87142abf55e7b9c91598
0-don <don.cryptus@gmail.com>
2026-01-13 00:05:45 +0100

    refactor: remove ReactQueryStreamedHydration and simplify QueryProvider component

--- src/components/provider/query-provider.tsx
+++ src/components/provider/query-provider.tsx
@@ -1,17 +1,16 @@
 "use client";
 
+import getQueryClient from "@/lib/react-query/client";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { ReactQueryStreamedHydration } from "@tanstack/react-query-next-experimental";
 import { ReactNode } from "react";
-import getQueryClient from "@/lib/react-query/client";
 
 export function QueryProvider({ children }: { children: ReactNode }) {
   const queryClient = getQueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>
-      <ReactQueryStreamedHydration>{children}</ReactQueryStreamedHydration>
+      {children}
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
