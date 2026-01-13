1fba345b4ea86e9da8c3abf10d8a87292a352f15
0-don <don.cryptus@gmail.com>
2026-01-14 00:28:43 +0100

    feat: add icons section to page metadata for improved SEO

--- src/lib/config/metadata.ts
+++ src/lib/config/metadata.ts
@@ -55,6 +55,9 @@ export async function getPageMetadata(
         },
       ],
     },
+    icons: {
+      icon: "/images/logo.gif",
+    },
     robots: {
       index: shouldIndex,
       follow: shouldIndex,
