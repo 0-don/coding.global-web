b968016d64be109e54606de765c00c3d8352bda8
0-don <don.cryptus@gmail.com>
2026-01-20 18:02:05 +0100

    fix: streamline error handling in Error component and improve PostHog initialization

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -4,4 +4,8 @@ posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
   api_host: "/ingest",
   ui_host: "https://eu.posthog.com",
   defaults: "2025-05-24",
+  capture_pageview: "history_change",
+  capture_pageleave: "if_capture_pageview",
+  capture_performance: true,
+  capture_heatmaps: true,
 });
