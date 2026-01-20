468dca32344dc0241b7ecf648cce08c6e4588d85
0-don <don.cryptus@gmail.com>
2026-01-20 17:32:46 +0100

    feat: add PostHog integration with proxy configuration and update environment types

--- src/components/provider/posthog-provider.tsx
+++ src/components/provider/posthog-provider.tsx
@@ -8,7 +8,8 @@ import { PostHogProvider as PHProvider } from "posthog-js/react";
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
-      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+      api_host: process.env.NEXT_PUBLIC_POSTHOG_PROXY_HOST,
+      ui_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
       defaults: "2025-11-30",
     });
   }, []);
