c2674f460e7b65c0e524719317527ee1e54e946c
0-don <don.cryptus@gmail.com>
2026-01-22 19:04:38 +0100

    fix: enhance PostHog integration by adding page view tracking and updating reverse proxy headers

--- src/components/provider/posthog-provider.tsx
+++ src/components/provider/posthog-provider.tsx
@@ -1,9 +1,29 @@
 "use client";
 
 import { useSessionHook } from "@/hook/session-hook";
+import { usePathname, useSearchParams } from "next/navigation";
 import posthog from "posthog-js";
 import { PostHogProvider as PHProvider } from "posthog-js/react";
 import { useEffect, useRef } from "react";
+
+function PostHogPageView() {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    if (pathname) {
+      let url = window.origin + pathname;
+      if (searchParams && searchParams.toString()) {
+        url = url + `?${searchParams.toString()}`;
+      }
+      posthog.capture("$pageview", {
+        $current_url: url,
+      });
+    }
+  }, [pathname, searchParams]);
+
+  return null;
+}
 
 function PostHogIdentify() {
   const session = useSessionHook();
@@ -35,6 +55,7 @@ function PostHogIdentify() {
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
   return (
     <PHProvider client={posthog}>
+      <PostHogPageView />
       <PostHogIdentify />
       {children}
     </PHProvider>
