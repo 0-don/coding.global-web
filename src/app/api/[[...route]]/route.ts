61eb865d468bb13abf3b9e3bf7e89a333d9ef8e7
0-don <don.cryptus@gmail.com>
2026-01-31 17:32:49 +0100

    feat: integrate OpenTelemetry for enhanced logging and tracing support

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -2,9 +2,29 @@ import { botRoute } from "@/server/bot/route";
 import { chatRoute } from "@/server/chat/route";
 import { searchRoute } from "@/server/search/route";
 import { terminalRoute } from "@/server/terminal/route";
+import { opentelemetry } from "@elysiajs/opentelemetry";
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-proto";
+import { BatchSpanProcessor } from "@opentelemetry/sdk-trace-node";
 import { Elysia } from "elysia";
 
 export const app = new Elysia({ prefix: "/api" })
+  .use(
+    process.env.NEXT_PUBLIC_POSTHOG_KEY
+      ? opentelemetry({
+          serviceName: "coding-global-web-api",
+          spanProcessors: [
+            new BatchSpanProcessor(
+              new OTLPTraceExporter({
+                url: "https://eu.i.posthog.com/i/v1/traces",
+                headers: {
+                  Authorization: `Bearer ${process.env.NEXT_PUBLIC_POSTHOG_KEY}`,
+                },
+              }),
+            ),
+          ],
+        })
+      : (app: Elysia) => app,
+  )
   .use(chatRoute)
   .use(botRoute)
   .use(searchRoute)
