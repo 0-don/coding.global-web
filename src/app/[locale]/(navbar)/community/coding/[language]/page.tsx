aae158965cc257b0404b71d6d40c2f2d776ca8df
0-don <don.cryptus@gmail.com>
2026-05-09 20:15:28 +0200

    fix: drop generateStaticParams to make coding pages fully dynamic (SessionProvider needs headers)

--- src/app/[locale]/(navbar)/community/coding/[language]/page.tsx
+++ src/app/[locale]/(navbar)/community/coding/[language]/page.tsx
@@ -5,18 +5,12 @@ import { getPageMetadata } from "@/lib/config/metadata";
 import getQueryClient from "@/lib/react-query/client";
 import { queryKeys } from "@/lib/react-query/keys";
 import { rpc } from "@/lib/rpc";
-import { PROGRAMMING_LANGUAGES, ProgrammingThreadType } from "@/lib/types";
+import { ProgrammingThreadType } from "@/lib/types";
 import { handleElysia } from "@/lib/utils/base";
 import { getCookieValue, serverLocale } from "@/lib/utils/server";
 import { ThreadState, getThreadStoreKey } from "@/store/thread-store";
 import { HydrationBoundary, dehydrate } from "@tanstack/react-query";
 import { getTranslations } from "next-intl/server";
-
-export async function generateStaticParams() {
-  return PROGRAMMING_LANGUAGES.map((language) => ({
-    language,
-  }));
-}
 
 export async function generateMetadata(props: {
   params: Promise<{ locale: string; language: ProgrammingThreadType }>;
