71ab861997dd3f089e45bb305436922e7cdd3a8a
0-don <don.cryptus@gmail.com>
2026-06-14 00:35:51 +0200

    chore: migrate coding.global-web to coding-global.com

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -2,7 +2,7 @@ import posthog from "posthog-js";
 
 if (process.env.NODE_ENV !== "development") {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
-    api_host: "https://ph.coding.global",
+    api_host: "https://ph.coding-global.com",
     ui_host: "https://eu.posthog.com",
     defaults: "2025-05-24",
     capture_performance: true,
